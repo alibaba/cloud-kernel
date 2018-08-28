795508c24d910fdc89da8d0feefa3bdc7f609b52
Julien Thierry <julien.thierry@arm.com>
2018-08-28 16:51:15 +0100

    arm64: Use daifflag_restore after bp_hardening
    OpenAnolis Bug Tracker: 0000546
    
    commit 9a0c032825e073e002ee80cf2577431d7296a7f7 upstream.
    
    For EL0 entries requiring bp_hardening, daif status is kept at
    DAIF_PROCCTX_NOIRQ until after hardening has been done. Then interrupts
    are enabled through local_irq_enable().
    
    Before using local_irq_* functions, daifflags should be properly restored
    to a state where IRQs are enabled.
    
    Enable IRQs by restoring DAIF_PROCCTX state after bp hardening.
    
    Acked-by: James Morse <james.morse@arm.com>
    Signed-off-by: Julien Thierry <julien.thierry@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Zou Cao <zoucao@linux.alibaba.com>
    Reviewed-by: luanshi <zhangliguang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -39,6 +39,7 @@
 #include <asm/cmpxchg.h>
 #include <asm/cpufeature.h>
 #include <asm/exception.h>
+#include <asm/daifflags.h>
 #include <asm/debug-monitors.h>
 #include <asm/esr.h>
 #include <asm/sysreg.h>
@@ -759,7 +760,7 @@ asmlinkage void __exception do_el0_ia_bp_hardening(unsigned long addr,
 	if (addr > TASK_SIZE)
 		arm64_apply_bp_hardening();
 
-	local_irq_enable();
+	local_daif_restore(DAIF_PROCCTX);
 	do_mem_abort(addr, esr, regs);
 }
 
@@ -773,7 +774,7 @@ asmlinkage void __exception do_sp_pc_abort(unsigned long addr,
 	if (user_mode(regs)) {
 		if (instruction_pointer(regs) > TASK_SIZE)
 			arm64_apply_bp_hardening();
-		local_irq_enable();
+		local_daif_restore(DAIF_PROCCTX);
 	}
 
 	clear_siginfo(&info);
