726dd76028c44ba311df281434a14e5fc73d709c
Marc Zyngier <marc.zyngier@arm.com>
2018-09-27 17:15:34 +0100

    arm64: arch_timerq: Add workaround for ARM erratum 1188873
    task #28924046
    
    [ Upstream commit 95b861a4a6d94f64d5242605569218160ebacdbe ]
    
    When running on Cortex-A76, a timer access from an AArch32 EL0
    task may end up with a corrupted value or register. The workaround for
    this is to trap these accesses at EL1/EL2 and execute them there.
    
    This only affects versions r0p0, r1p0 and r2p0 of the CPU.
    
    Backport change:
    The patch modifies ARM64_WORKAROUND_1188873 from 35 to 36 and
    the ARM_CPU_PART_CORTEX_A76 is deleted because a previous patch
    has been modified.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Bin Yu <jkchen@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: zou cao <zoucao@linux.alibaba.com>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -54,7 +54,8 @@
 #define ARM64_WORKAROUND_1463225		33
 #define ARM64_SSBS				34
 #define ARM64_HAS_CRC32			35
+#define ARM64_WORKAROUND_1188873		36
 
-#define ARM64_NCAPS				36
+#define ARM64_NCAPS				37
 
 #endif /* __ASM_CPUCAPS_H */
