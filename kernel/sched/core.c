800bf05db86a2e29b56e4c3b2644e62ffa00d059
Daniel Lezcano <daniel.lezcano@linaro.org>
2018-10-04 14:04:03 +0200

    cpuidle: menu: Remove get_loadavg() from the performance multiplier
    to #28739709
    
    commit a7fe5190c03f8137ef08db84a58dd4daf2c4785d upstream
    
    The function get_loadavg() returns almost always zero. To be more
    precise, statistically speaking for a total of 1023379 times passing
    in the function, the load is equal to zero 1020728 times, greater than
    100, 610 times, the remaining is between 0 and 5.
    
    In 2011, the get_loadavg() was removed from the Android tree because
    of the above [1]. At this time, the load was:
    
    unsigned long this_cpu_load(void)
    {
            struct rq *this = this_rq();
            return this->cpu_load[0];
    }
    
    In 2014, the code was changed by commit 372ba8cb46b2 (cpuidle: menu: Lookup CPU
    runqueues less) and the load is:
    
    void get_iowait_load(unsigned long *nr_waiters, unsigned long *load)
    {
            struct rq *rq = this_rq();
            *nr_waiters = atomic_read(&rq->nr_iowait);
            *load = rq->load.weight;
    }
    
    with the same result.
    
    Both measurements show using the load in this code path does no matter
    anymore. Removing it.
    
    [1] https://android.googlesource.com/kernel/common/+/4dedd9f124703207895777ac6e91dacde0f7cc17
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>
    Reviewed-by: Shanpei Chen <shanpeic@linux.alibaba.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2886,13 +2886,6 @@ unsigned long nr_iowait_cpu(int cpu)
 	return atomic_read(&this->nr_iowait);
 }
 
-void get_iowait_load(unsigned long *nr_waiters, unsigned long *load)
-{
-	struct rq *rq = this_rq();
-	*nr_waiters = atomic_read(&rq->nr_iowait);
-	*load = rq->load.weight;
-}
-
 #ifdef CONFIG_SMP
 
 /*
