275561cd90798f79988ed5db943fcf580fde79fd
Randy Dunlap <rdunlap@infradead.org>
2018-11-04 18:40:14 -0800

    resource/docs: Fix new kernel-doc warnings
    commit f75d651587f719a813ebbbfeee570e6570731d55 upstream.
    
    The first group of warnings is caused by a "/**" kernel-doc notation
    marker but the function comments are not in kernel-doc format.
    Also add another error return value here.
    
      ../kernel/resource.c:337: warning: Function parameter or member 'start' not described in 'find_next_iomem_res'
      ../kernel/resource.c:337: warning: Function parameter or member 'end' not described in 'find_next_iomem_res'
      ../kernel/resource.c:337: warning: Function parameter or member 'flags' not described in 'find_next_iomem_res'
      ../kernel/resource.c:337: warning: Function parameter or member 'desc' not described in 'find_next_iomem_res'
      ../kernel/resource.c:337: warning: Function parameter or member 'first_lvl' not described in 'find_next_iomem_res'
      ../kernel/resource.c:337: warning: Function parameter or member 'res' not described in 'find_next_iomem_res'
    
    Add the missing function parameter documentation for the other warnings:
    
      ../kernel/resource.c:409: warning: Function parameter or member 'arg' not described in 'walk_iomem_res_desc'
      ../kernel/resource.c:409: warning: Function parameter or member 'func' not described in 'walk_iomem_res_desc'
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Fixes: b69c2e20f6e4 ("resource: Clean it up a bit")
    Link: http://lkml.kernel.org/r/dda2e4d8-bedd-3167-20fe-8c7d2d35b354@infradead.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [joseph: fix find_next_iomem_res() documentation]
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Caspar Zhang <caspar@linux.alibaba.com>

--- kernel/resource.c
+++ kernel/resource.c
@@ -390,7 +390,9 @@ static int __walk_iomem_res_desc(resource_size_t start, resource_size_t end,
 }
 
 /**
- * Walks through iomem resources and calls func() with matching resource
+ * walk_iomem_res_desc - walk through iomem resources
+ *
+ * Walks through iomem resources and calls @func() with matching resource
  * ranges. This walks through whole tree and not just first level children.
  * All the memory ranges which overlap start,end and also match flags and
  * desc are valid candidates.
@@ -399,6 +401,8 @@ static int __walk_iomem_res_desc(resource_size_t start, resource_size_t end,
  * @flags: I/O resource flags
  * @start: start addr
  * @end: end addr
+ * @arg: function argument for the callback @func
+ * @func: callback function that is called for each qualifying resource area
  *
  * NOTE: For a new descriptor search, define a new IORES_DESC in
  * <linux/ioport.h> and set it in 'desc' of a target resource entry.
