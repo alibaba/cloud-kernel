7069c85179b5b258a4f24b28bfc5ae9a351e1800
Jens Axboe <axboe@kernel.dk>
2018-11-27 17:07:17 -0700

    blk-mq: fix failure to decrement plug count on single rq removal
    to #29139300
    
    commit 4711b57317f0ff5ca9fbd5e2df6c73b2c07ddc53 upstream
    
    If we yank a 'same_queue_rq' request off the plug list, we should
    also decrement the cached request count.
    
    Fixes: 5f0ed774ed29 ("block: sum requests in the plug structure")
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Hongnan Li <hongnan.li@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1986,8 +1986,10 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 		 */
 		if (list_empty(&plug->mq_list))
 			same_queue_rq = NULL;
-		if (same_queue_rq)
+		if (same_queue_rq) {
 			list_del_init(&same_queue_rq->queuelist);
+			plug->rq_count--;
+		}
 		blk_add_rq_to_plug(plug, rq);
 
 		blk_mq_put_ctx(data.ctx);
