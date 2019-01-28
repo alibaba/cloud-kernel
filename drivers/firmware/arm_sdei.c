1395a807e6de398fc321d59852c80690274718b9
Xiongfeng Wang <wangxiongfeng2@huawei.com>
2019-01-28 15:36:48 +0800

    openeuler: firmware: arm_sdei: make 'sdei_api_event_disable/enable' public
    commit ac271e1a6b2498fcfebd3a56e00993727da6008a openEuler-1.0.
    
    NMI Watchdog need to enable the event for each core individually. But the
    existing public api 'sdei_event_enable' enable events for all cores when
    the event type is private.
    
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Xin Hao <haoxing990@gmail.com>
    Reviewed-by: Baolin Wang <baolin.wang@openanolis.org>

--- drivers/firmware/arm_sdei.c
+++ drivers/firmware/arm_sdei.c
@@ -390,7 +390,7 @@ static int sdei_platform_reset(void)
 	return err;
 }
 
-static int sdei_api_event_enable(u32 event_num)
+int sdei_api_event_enable(u32 event_num)
 {
 	return invoke_sdei_fn(SDEI_1_0_FN_SDEI_EVENT_ENABLE, event_num, 0, 0, 0,
 			      0, NULL);
@@ -435,7 +435,7 @@ int sdei_event_enable(u32 event_num)
 }
 EXPORT_SYMBOL(sdei_event_enable);
 
-static int sdei_api_event_disable(u32 event_num)
+int sdei_api_event_disable(u32 event_num)
 {
 	return invoke_sdei_fn(SDEI_1_0_FN_SDEI_EVENT_DISABLE, event_num, 0, 0,
 			      0, 0, NULL);
