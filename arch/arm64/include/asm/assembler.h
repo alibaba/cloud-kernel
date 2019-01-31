d458d876e7438a182c96d8fb48015f19a4f9e9f6
Julien Thierry <julien.thierry@arm.com>
2019-01-31 14:58:40 +0000

    arm64: Remove unused daif related functions/macros
    task #25552995
    
    commit a82785a953e03444fe38616aed4d27b01da79a97 upstream
    
    There are some helpers to modify PSR.[DAIF] bits that are not referenced
    anywhere. The less these bits are available outside of local_irq_*
    functions the better.
    
    Get rid of those unused helpers.
    
    Signed-off-by: Julien Thierry <julien.thierry@arm.com>
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Zou Cao <zoucao@linux.alibaba.com>
    Reviewed-by: luanshi <zhangliguang@linux.alibaba.com>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -60,16 +60,8 @@
 	.endm
 
 /*
- * Enable and disable interrupts.
+ * Save/restore interrupts.
  */
-	.macro	disable_irq
-	msr	daifset, #2
-	.endm
-
-	.macro	enable_irq
-	msr	daifclr, #2
-	.endm
-
 	.macro	save_and_disable_irq, flags
 	mrs	\flags, daif
 	msr	daifset, #2
