aff365b61f4315df36b1030fe870ee5cd01b31ac
George Zhang <georgezhang@linux.alibaba.com>
2019-03-15 11:17:17 +0800

    net: kernel hookers service for toa module
    LVS fullnat will replace network traffic's source ip with its local ip,
    and thus the backend servers cannot obtain the real client ip.
    
    To solve this, LVS has introduced the tcp option address (TOA) to store
    the essential ip address information in the last tcp ack packet of the
    3-way handshake, and the backend servers need to retrieve it from the
    packet header.
    
    In this patch, we have introduced the sk_toa_data member in the sock
    structure to hold the TOA information. There used to be an in-tree
    module for TOA managing, whereas it has now been maintained as an
    standalone module.
    
    In this case, the toa module should register its hook function(s) using
    the provided interfaces in the hookers module.
    
    TOA in sock structure:
    
    	__be32 sk_toa_data[16];
    
    The hookers module only provides the sk_toa_data placeholder, and the
    toa module can use this variable through the layout it needs.
    
    Hook interfaces:
    
    The hookers module replaces the kernel's syn_recv_sock and getname
    handler with a stub that chains the toa module's hook function(s) to the
    original handling function. The hookers module allows hook functions to
    be installed and uninstalled in any order.
    
    toa module:
    
    The external toa module will be provided in separate RPM package.
    
    [xuyu@linux.alibaba.com: amend commit log]
    
    Signed-off-by: George Zhang <georgezhang@linux.alibaba.com>
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Caspar Zhang <caspar@linux.alibaba.com>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -77,8 +77,8 @@ static void	tcp_v6_reqsk_send_ack(const struct sock *sk, struct sk_buff *skb,
 
 static int	tcp_v6_do_rcv(struct sock *sk, struct sk_buff *skb);
 
-static const struct inet_connection_sock_af_ops ipv6_mapped;
-static const struct inet_connection_sock_af_ops ipv6_specific;
+const struct inet_connection_sock_af_ops ipv6_mapped;
+const struct inet_connection_sock_af_ops ipv6_specific;
 #ifdef CONFIG_TCP_MD5SIG
 static const struct tcp_sock_af_ops tcp_sock_ipv6_specific;
 static const struct tcp_sock_af_ops tcp_sock_ipv6_mapped_specific;
@@ -1681,7 +1681,7 @@ static struct timewait_sock_ops tcp6_timewait_sock_ops = {
 	.twsk_destructor = tcp_twsk_destructor,
 };
 
-static const struct inet_connection_sock_af_ops ipv6_specific = {
+const struct inet_connection_sock_af_ops ipv6_specific = {
 	.queue_xmit	   = inet6_csk_xmit,
 	.send_check	   = tcp_v6_send_check,
 	.rebuild_header	   = inet6_sk_rebuild_header,
@@ -1700,6 +1700,7 @@ static const struct inet_connection_sock_af_ops ipv6_specific = {
 #endif
 	.mtu_reduced	   = tcp_v6_mtu_reduced,
 };
+EXPORT_SYMBOL(ipv6_specific);
 
 #ifdef CONFIG_TCP_MD5SIG
 static const struct tcp_sock_af_ops tcp_sock_ipv6_specific = {
@@ -1712,7 +1713,7 @@ static const struct tcp_sock_af_ops tcp_sock_ipv6_specific = {
 /*
  *	TCP over IPv4 via INET6 API
  */
-static const struct inet_connection_sock_af_ops ipv6_mapped = {
+const struct inet_connection_sock_af_ops ipv6_mapped = {
 	.queue_xmit	   = ip_queue_xmit,
 	.send_check	   = tcp_v4_send_check,
 	.rebuild_header	   = inet_sk_rebuild_header,
@@ -1730,6 +1731,7 @@ static const struct inet_connection_sock_af_ops ipv6_mapped = {
 #endif
 	.mtu_reduced	   = tcp_v4_mtu_reduced,
 };
+EXPORT_SYMBOL(ipv6_mapped);
 
 #ifdef CONFIG_TCP_MD5SIG
 static const struct tcp_sock_af_ops tcp_sock_ipv6_mapped_specific = {
