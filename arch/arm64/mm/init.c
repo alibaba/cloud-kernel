49a8cb88bf8f1adf8983c6b78d38175265e3d6c4
Chen Zhou <chenzhou10@huawei.com>
2019-04-17 16:22:02 +0800

    openeuler: arm64: kdump: support reserving crashkernel above 4G
    OpenAnolis Bug Tracker: 0000570
    
    commit 19ab5d6d6b8c9dc25b6cccba582fbd0a6211297a openEuler
    
    hulk inclusion
    category: feature
    bugzilla: 12800
    CVE: NA
    
    -------------------------------------------------
    
    When crashkernel is reserved above 4G in memory, kernel should
    reserve some amount of low memory for swiotlb and some DMA buffers.
    
    Kernel would try to allocate at least 256M below 4G automatically
    as x86_64 if crashkernel is above 4G. Meanwhile, support
    crashkernel=X,[high,low] in arm64.
    
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Reviewed-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Acked-by: Zou Cao <zoucao@linux.alibaba.com>
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -169,8 +169,10 @@ static void __init reserve_crashkernel(void)
 
 	if (crash_base == 0) {
 		/* Current arm64 boot protocol requires 2MB alignment */
-		crash_base = memblock_find_in_range(0, ARCH_LOW_ADDRESS_LIMIT,
-				crash_size, SZ_2M);
+		crash_base = memblock_find_in_range(0,
+				high ? memblock_end_of_DRAM()
+				: ARCH_LOW_ADDRESS_LIMIT,
+				crash_size, CRASH_ALIGN);
 		if (crash_base == 0) {
 			pr_warn("cannot allocate crashkernel (size:0x%llx)\n",
 				crash_size);
@@ -188,12 +190,17 @@ static void __init reserve_crashkernel(void)
 			return;
 		}
 
-		if (!IS_ALIGNED(crash_base, SZ_2M)) {
+		if (!IS_ALIGNED(crash_base, CRASH_ALIGN)) {
 			pr_warn("cannot reserve crashkernel: base address is not 2MB aligned\n");
 			return;
 		}
 	}
 	memblock_reserve(crash_base, crash_size);
+
+	if (crash_base >= SZ_4G && reserve_crashkernel_low()) {
+		memblock_free(crash_base, crash_size);
+		return;
+	}
 
 	pr_info("crashkernel reserved: 0x%016llx - 0x%016llx (%lld MB)\n",
 		crash_base, crash_base + crash_size, crash_size >> 20);
