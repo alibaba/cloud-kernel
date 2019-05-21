47ac0d289e3e420649ad1db325edf90f55b47f8a
Hou Tao <houtao1@huawei.com>
2019-05-21 15:59:03 +0800

    block: make rq sector size accessible for block stats
    to #29361128
    
    commit 3d24430694077313c75c6b89f618db09943621e4 upstream.
    
    Currently rq->data_len will be decreased by partial completion or
    zeroed by completion, so when blk_stat_add() is invoked, data_len
    will be zero and there will never be samples in poll_cb because
    blk_mq_poll_stats_bkt() will return -1 if data_len is zero.
    
    We could move blk_stat_add() back to __blk_mq_complete_request(),
    but that would make the effort of trying to call ktime_get_ns()
    once in vain. Instead we can reuse throtl_size field, and use
    it for both block stats and block throttle, and adjust the
    logic in blk_mq_poll_stats_bkt() accordingly.
    
    Fixes: 4bc6339a583c ("block: move blk_stat_add() to __blk_mq_end_request()")
    Tested-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Jiufei Xue <jiufei.xue@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -3011,9 +3011,7 @@ void blk_start_request(struct request *req)
 
 	if (test_bit(QUEUE_FLAG_STATS, &req->q->queue_flags)) {
 		req->io_start_time_ns = ktime_get_ns();
-#ifdef CONFIG_BLK_DEV_THROTTLING_LOW
-		req->throtl_size = blk_rq_sectors(req);
-#endif
+		req->stats_sectors = blk_rq_sectors(req);
 		req->rq_flags |= RQF_STATS;
 		rq_qos_issue(req->q, req);
 	}
