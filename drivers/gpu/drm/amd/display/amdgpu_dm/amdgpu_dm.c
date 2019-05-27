696441b62d345ee333acb0a16f2c9e13994f3f87
Emily Deng <Emily.Deng@amd.com>
2019-05-27 11:12:51 +0800

    drm/amdgpu: fix unload driver fail
    fix #29035007
    
    commit c8bdf2b63e5b6b31b3b4826b8e87c0c2f6b650ff upstream
    
    dc_destroy should be called amdgpu_cgs_destroy_device,
    as it will use cgs context to read or write registers.
    
    Signed-off-by: Emily Deng <Emily.Deng@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Acked-by: Caspar Zhang <caspar@linux.alibaba.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -496,6 +496,10 @@ error:
 static void amdgpu_dm_fini(struct amdgpu_device *adev)
 {
 	amdgpu_dm_destroy_drm_device(&adev->dm);
+
+	/* DC Destroy TODO: Replace destroy DAL */
+	if (adev->dm.dc)
+		dc_destroy(&adev->dm.dc);
 	/*
 	 * TODO: pageflip, vlank interrupt
 	 *
@@ -510,9 +514,6 @@ static void amdgpu_dm_fini(struct amdgpu_device *adev)
 		mod_freesync_destroy(adev->dm.freesync_module);
 		adev->dm.freesync_module = NULL;
 	}
-	/* DC Destroy TODO: Replace destroy DAL */
-	if (adev->dm.dc)
-		dc_destroy(&adev->dm.dc);
 	return;
 }
 
