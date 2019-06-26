428adfc04a2a8d9b25301779bcbc6bde58001430
Jeremy Linton <jeremy.linton@arm.com>
2019-06-26 16:37:16 -0500

    ACPI/PPTT: Add function to return ACPI 6.3 Identical tokens
    to #25688970
    
    commit 56855a99f3d0d1e9f1f4e24f5851f9bf14c83296 upstream
    
    ACPI 6.3 adds a flag to indicate that child nodes are all
    identical cores. This is useful to authoritatively determine
    if a set of (possibly offline) cores are identical or not.
    
    Since the flag doesn't give us a unique id we can generate
    one and use it to create bitmaps of sibling nodes, or simply
    in a loop to determine if a subset of cores are identical.
    
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Tested-by: Hanjun Guo <hanjun.guo@linaro.org>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: luanshi <zhangliguang@linux.alibaba.com>
    Acked-by: zou cao <zoucao@linux.alibaba.com>
    Acked-by: Caspar Zhang <caspar@linux.alibaba.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1303,6 +1303,7 @@ static inline int lpit_read_residency_count_address(u64 *address)
 int acpi_pptt_cpu_is_thread(unsigned int cpu);
 int find_acpi_cpu_topology(unsigned int cpu, int level);
 int find_acpi_cpu_topology_package(unsigned int cpu);
+int find_acpi_cpu_topology_hetero_id(unsigned int cpu);
 int find_acpi_cpu_cache_topology(unsigned int cpu, int level);
 #else
 static inline int acpi_pptt_cpu_is_thread(unsigned int cpu)
@@ -1314,6 +1315,10 @@ static inline int find_acpi_cpu_topology(unsigned int cpu, int level)
 	return -EINVAL;
 }
 static inline int find_acpi_cpu_topology_package(unsigned int cpu)
+{
+	return -EINVAL;
+}
+static inline int find_acpi_cpu_topology_hetero_id(unsigned int cpu)
 {
 	return -EINVAL;
 }
