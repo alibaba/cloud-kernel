2617bd085b06337f1cb417e07c2070a68f2a5814
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2019-08-22 21:45:18 -0700

    null_blk: create a helper for zoned devices
    OpenAnolis Bug Tracker: 0000557
    
    commit fceb5d1b19cbe6263f09dbe8e8138edf02eb6e6b upstream.
    
    This patch creates a helper function for handling zoned block device
    operations.
    
    This patch also restructured the code for null_blk_zoned.c and uses the
    pattern to return blk_status_t and catch the error in the function
    null_handle_cmd() into cmd->error variable instead of setting it up in
    the deeper layer just like the way it is done for flush, badblocks and
    memory backed case in the null_handle_cmd(). We also move
    null_handle_zoned() to the null_blk_zoned.c to keep the zoned code
    separate.
    
    [Joseph] fold commit 38b4e09fbcca ("null_blk: fix inline misuse") for
    cbc build consideration.
    
    Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Acked-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- drivers/block/null_blk_main.c
+++ drivers/block/null_blk_main.c
@@ -1251,12 +1251,8 @@ static blk_status_t null_handle_cmd(struct nullb_cmd *cmd)
 			nr_sectors = blk_rq_sectors(cmd->rq);
 		}
 
-		if (op == REQ_OP_WRITE)
-			null_zone_write(cmd, sector, nr_sectors);
-		else if (op == REQ_OP_ZONE_RESET)
-			null_zone_reset(cmd, sector);
-		else if (op == REQ_OP_ZONE_RESET_ALL)
-			null_zone_reset(cmd, 0);
+		cmd->error = null_handle_zoned(cmd, op, sector, nr_sectors);
+
 	}
 out:
 	/* Complete IO by inline, softirq or timer */
