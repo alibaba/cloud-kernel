5a97f9cbe98ba98bf5ce931fc5742211b45e3564
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2019-10-18 20:20:27 -0700

    proc/meminfo: fix output alignment
    OpenAnolis Bug Tracker: 0000366
    
    commit 2be5fbf9a91c417cd178a481538baddda3e2f38c upstream.
    
    Patch series "Fixes for THP in page cache", v2.
    
    This patch (of 5):
    
    Add extra space for FileHugePages and FilePmdMapped, so the output is
    aligned with other rows.
    
    Link: http://lkml.kernel.org/r/20191017164223.2762148-2-songliubraving@fb.com
    Fixes: 60fbf0ab5da1 ("mm,thp: stats for file backed THP")
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Tested-by: Song Liu <songliubraving@fb.com>
    Acked-by: Yang Shi <yang.shi@linux.alibaba.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Cc: William Kucharski <william.kucharski@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Gang Deng <gavin.dg@linux.alibaba.com>

--- fs/proc/meminfo.c
+++ fs/proc/meminfo.c
@@ -153,9 +153,9 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 	show_val_kb(m, "AnonHugePages:  ", ext.anon_thps * HPAGE_PMD_NR);
 	show_val_kb(m, "ShmemHugePages: ", ext.shmem_thps * HPAGE_PMD_NR);
 	show_val_kb(m, "ShmemPmdMapped: ", ext.shmem_pmd_mapped * HPAGE_PMD_NR);
-	show_val_kb(m, "FileHugePages: ",
+	show_val_kb(m, "FileHugePages:  ",
 		    global_node_page_state(NR_FILE_THPS) * HPAGE_PMD_NR);
-	show_val_kb(m, "FilePmdMapped: ",
+	show_val_kb(m, "FilePmdMapped:  ",
 		    global_node_page_state(NR_FILE_PMDMAPPED) * HPAGE_PMD_NR);
 #endif
 
