1d1b4c6cabd611699fd75d58a6331950b8de6500
Yang Shi <yang.shi@linux.alibaba.com>
2019-10-22 04:54:20 +0800

    mm: thp: move deferred split queue to memcg's nodeinfo
    The commit 87eaceb3faa59b9b4d940ec9554ce251325d83fe ("mm: thp: make
    deferred split shrinker memcg aware") makes deferred split queue per
    memcg to resolve memcg pre-mature OOM problem.  But, all nodes end up
    sharing the same queue instead of one queue per-node before the commit.
    It is not a big deal for memcg limit reclaim, but it may cause global
    kswapd shrink THPs from a different node.
    
    And, 0-day testing reported -19.6% regression of stress-ng's madvise
    test [1].  I didn't see that much regression on my test box (24 threads,
    48GB memory, 2 nodes), with the same test (stress-ng --timeout 1
    --metrics-brief --sequential 72  --class vm --exclude spawn,exec), I saw
    average -3% (run the same test 10 times then calculate the average since
    the test itself may have most 15% variation according to my test)
    regression sometimes (not every time, sometimes I didn't see regression
    at all).
    
    This might be caused by deferred split queue lock contention.  With some
    configuration (i.e. just one root memcg) the lock contention my be worse
    than before (given 2 nodes, two locks are reduced to one lock).
    
    So, moving deferred split queue to memcg's nodeinfo to make it NUMA
    aware again.
    
    With this change stress-ng's madvise test shows average 4% improvement
    sometimes and I didn't see degradation anymore.
    
    [1]: https://lore.kernel.org/lkml/20190930084604.GC17687@shao2-debian/
    
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Kirill Tkhai <ktkhai@virtuozzo.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Yang Shi <yang.shi@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5006,6 +5006,12 @@ static int alloc_mem_cgroup_per_node_info(struct mem_cgroup *memcg, int node)
 	pn->on_tree = false;
 	pn->memcg = memcg;
 
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+	spin_lock_init(&pn->deferred_split_queue.split_queue_lock);
+	INIT_LIST_HEAD(&pn->deferred_split_queue.split_queue);
+	pn->deferred_split_queue.split_queue_len = 0;
+#endif
+
 	memcg->nodeinfo[node] = pn;
 	return 0;
 }
@@ -5084,11 +5090,6 @@ static struct mem_cgroup *mem_cgroup_alloc(void)
 	INIT_LIST_HEAD(&memcg->cgwb_list);
 #endif
 	kidled_memcg_init(memcg);
-#ifdef CONFIG_TRANSPARENT_HUGEPAGE
-	spin_lock_init(&memcg->deferred_split_queue.split_queue_lock);
-	INIT_LIST_HEAD(&memcg->deferred_split_queue.split_queue);
-	memcg->deferred_split_queue.split_queue_len = 0;
-#endif
 	idr_replace(&mem_cgroup_idr, memcg, memcg->id.id);
 	return memcg;
 fail:
@@ -5425,6 +5426,8 @@ static int mem_cgroup_move_account(struct page *page,
 	unsigned int nr_pages = compound ? hpage_nr_pages(page) : 1;
 	int ret;
 	bool anon;
+	struct deferred_split *ds_queue;
+	int nid = page_to_nid(page);
 
 	VM_BUG_ON(from == to);
 	VM_BUG_ON_PAGE(PageLRU(page), page);
@@ -5472,10 +5475,11 @@ static int mem_cgroup_move_account(struct page *page,
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	if (compound && !list_empty(page_deferred_list(page))) {
-		spin_lock(&from->deferred_split_queue.split_queue_lock);
+		ds_queue = &from->nodeinfo[nid]->deferred_split_queue;
+		spin_lock(&ds_queue->split_queue_lock);
 		list_del_init(page_deferred_list(page));
-		from->deferred_split_queue.split_queue_len--;
-		spin_unlock(&from->deferred_split_queue.split_queue_lock);
+		ds_queue->split_queue_len--;
+		spin_unlock(&ds_queue->split_queue_lock);
 	}
 #endif
 	/*
@@ -5489,11 +5493,12 @@ static int mem_cgroup_move_account(struct page *page,
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	if (compound && list_empty(page_deferred_list(page))) {
-		spin_lock(&to->deferred_split_queue.split_queue_lock);
+		ds_queue = &to->nodeinfo[nid]->deferred_split_queue;
+		spin_lock(&ds_queue->split_queue_lock);
 		list_add_tail(page_deferred_list(page),
-			      &to->deferred_split_queue.split_queue);
-		to->deferred_split_queue.split_queue_len++;
-		spin_unlock(&to->deferred_split_queue.split_queue_lock);
+			      &ds_queue->split_queue);
+		ds_queue->split_queue_len++;
+		spin_unlock(&ds_queue->split_queue_lock);
 	}
 #endif
 
