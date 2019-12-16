5fcab459fad0281d77c3e55abde90413be110d1d
Shakeel Butt <shakeelb@google.com>
2019-12-16 17:54:41 +0800

    mm, memcg: introduce memory.events.local
    commit 1e577f970f66a53d429cbee37b36177c9712f488 upstream.
    
    The memory controller in cgroup v2 exposes memory.events file for each
    memcg which shows the number of times events like low, high, max, oom
    and oom_kill have happened for the whole tree rooted at that memcg.
    Users can also poll or register notification to monitor the changes in
    that file.  Any event at any level of the tree rooted at memcg will
    notify all the listeners along the path till root_mem_cgroup.  There are
    existing users which depend on this behavior.
    
    However there are users which are only interested in the events
    happening at a specific level of the memcg tree and not in the events in
    the underlying tree rooted at that memcg.  One such use-case is a
    centralized resource monitor which can dynamically adjust the limits of
    the jobs running on a system.  The jobs can create their sub-hierarchy
    for their own sub-tasks.  The centralized monitor is only interested in
    the events at the top level memcgs of the jobs as it can then act and
    adjust the limits of the jobs.  Using the current memory.events for such
    centralized monitor is very inconvenient.  The monitor will keep
    receiving events which it is not interested and to find if the received
    event is interesting, it has to read memory.event files of the next
    level and compare it with the top level one.  So, let's introduce
    memory.events.local to the memcg which shows and notify for the events
    at the memcg level.
    
    Now, does memory.stat and memory.pressure need their local versions.  IMHO
    no due to the no internal process contraint of the cgroup v2.  The
    memory.stat file of the top level memcg of a job shows the stats and
    vmevents of the whole tree.  The local stats or vmevents of the top level
    memcg will only change if there is a process running in that memcg but v2
    does not allow that.  Similarly for memory.pressure there will not be any
    process in the internal nodes and thus no chance of local pressure.
    
    Link: http://lkml.kernel.org/r/20190527174643.209172-1-shakeelb@google.com
    Signed-off-by: Shakeel Butt <shakeelb@google.com>
    Reviewed-by: Roman Gushchin <guro@fb.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Chris Down <chris@chrisdown.name>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -244,8 +244,9 @@ struct mem_cgroup {
 	/* OOM-Killer disable */
 	int		oom_kill_disable;
 
-	/* memory.events */
+	/* memory.events and memory.events.local */
 	struct cgroup_file events_file;
+	struct cgroup_file events_local_file;
 
 	/* handle for "memory.swap.events" */
 	struct cgroup_file swap_events_file;
@@ -287,6 +288,7 @@ struct mem_cgroup {
 	atomic_long_t		stat[MEMCG_NR_STAT];
 	atomic_long_t		events[NR_VM_EVENT_ITEMS];
 	atomic_long_t memory_events[MEMCG_NR_MEMORY_EVENTS];
+	atomic_long_t		memory_events_local[MEMCG_NR_MEMORY_EVENTS];
 
 	unsigned long		socket_pressure;
 
@@ -796,6 +798,9 @@ static inline void count_memcg_event_mm(struct mm_struct *mm,
 static inline void memcg_memory_event(struct mem_cgroup *memcg,
 				      enum memcg_memory_event event)
 {
+	atomic_long_inc(&memcg->memory_events_local[event]);
+	cgroup_file_notify(&memcg->events_local_file);
+
 	do {
 		atomic_long_inc(&memcg->memory_events[event]);
 		cgroup_file_notify(&memcg->events_file);
