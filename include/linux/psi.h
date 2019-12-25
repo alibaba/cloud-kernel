106824ae9d3923193086d86a3a7d849a655de72c
Joseph Qi <joseph.qi@linux.alibaba.com>
2019-12-25 15:54:56 +0800

    alinux: introduce psi_v1 boot parameter
    Instead using static kconfig CONFIG_PSI_CGROUP_V1, we introduce a boot
    parameter psi_v1 to enable psi cgroup v1 support. Default it is
    disabled, which means when passing psi=1 boot parameter, we only support
    cgroup v2.
    This is to keep consistent with other cgroup v1 features such as cgroup
    writeback v1 (cgwb_v1).
    
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- include/linux/psi.h
+++ include/linux/psi.h
@@ -14,6 +14,8 @@ struct css_set;
 extern struct static_key_false psi_disabled;
 extern struct psi_group psi_system;
 
+extern struct static_key_false psi_v1_disabled;
+
 void psi_init(void);
 
 void psi_task_change(struct task_struct *task, int clear, int set);
