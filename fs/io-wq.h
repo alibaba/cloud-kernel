402703196c29705db0cb8395d3efd7e4bbe7b8a7
Jens Axboe <axboe@kernel.dk>
2020-02-08 19:16:39 -0700

    io-wq: add io_wq_cancel_pid() to cancel based on a specific pid
    to #26323588
    
    commit 36282881a795cbf717aca79392ae9cdf0fef59c9 upstream.
    
    Add a helper that allows the caller to cancel work based on what mm
    it belongs to. This allows io_uring to cancel work from a given
    task or thread when it exits.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Acked-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>

--- fs/io-wq.h
+++ fs/io-wq.h
@@ -75,6 +75,7 @@ struct io_wq_work {
 	const struct cred *creds;
 	struct fs_struct *fs;
 	unsigned flags;
+	pid_t task_pid;
 };
 
 #define INIT_IO_WORK(work, _func)			\
@@ -108,6 +109,7 @@ void io_wq_flush(struct io_wq *wq);
 
 void io_wq_cancel_all(struct io_wq *wq);
 enum io_wq_cancel io_wq_cancel_work(struct io_wq *wq, struct io_wq_work *cwork);
+enum io_wq_cancel io_wq_cancel_pid(struct io_wq *wq, pid_t pid);
 
 typedef bool (work_cancel_fn)(struct io_wq_work *, void *);
 
