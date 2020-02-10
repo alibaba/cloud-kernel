16a730c070f8543bf8266568a3f89c6c06d409ec
Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
2020-02-10 20:44:39 +0800

    crypto: rename sm3-256 to sm3 in hash_algo_name
    fix #26344811
    
    commit 6a30e1b1dcad0ba94fae757f797812d7d8dcb72c upstream
    
    The name sm3-256 is defined in hash_algo_name in hash_info, but the
    algorithm name implemented in sm3_generic.c is sm3, which will cause
    the sm3-256 algorithm to be not found in some application scenarios of
    the hash algorithm, and an ENOENT error will occur. For example,
    IMA, keys, and other subsystems that reference hash_algo_name all use
    the hash algorithm of sm3.
    
    Fixes: 5ca4c20cfd37 ("keys, trusted: select hash algorithm for TPM2 chips")
    Signed-off-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Pascal van Leeuwen <pvanleeuwen@rambus.com>
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>
    Signed-off-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Jia Zhang <zhang.jia@linux.alibaba.com>
    Acked-by: Caspar Zhang <caspar@linux.alibaba.com>

--- crypto/hash_info.c
+++ crypto/hash_info.c
@@ -31,7 +31,7 @@ const char *const hash_algo_name[HASH_ALGO__LAST] = {
 	[HASH_ALGO_TGR_128]	= "tgr128",
 	[HASH_ALGO_TGR_160]	= "tgr160",
 	[HASH_ALGO_TGR_192]	= "tgr192",
-	[HASH_ALGO_SM3_256]	= "sm3-256",
+	[HASH_ALGO_SM3_256]	= "sm3",
 };
 EXPORT_SYMBOL_GPL(hash_algo_name);
 
