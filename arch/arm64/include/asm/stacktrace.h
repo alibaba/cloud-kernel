46ad7da77a33c9a2301f3d79bb74ce0adc93d935
Torsten Duwe <duwe@lst.de>
2020-03-06 10:54:55 +0800

    arm64: reliable stacktraces
    cherry-picked from: https://patchwork.kernel.org/patch/10657429/
    
    Enhance the stack unwinder so that it reports whether it had to stop
    normally or due to an error condition; unwind_frame() will report
    continue/error/normal ending and walk_stackframe() will pass that
    info. __save_stack_trace() is used to check the validity of a stack;
    save_stack_trace_tsk_reliable() can now trivially be implemented.
    Modify arch/arm64/kernel/time.c as the only external caller so far
    to recognise the new semantics.
    
    I had to introduce a marker symbol kthread_return_to_user to tell
    the normal origin of a kernel thread.
    
    Signed-off-by: Torsten Duwe <duwe@suse.de>
    Signed-off-by: Zou Cao <zoucao@linux.alibaba.com>
    Acked-by: Caspar Zhang <caspar@linux.alibaba.com>

--- arch/arm64/include/asm/stacktrace.h
+++ arch/arm64/include/asm/stacktrace.h
@@ -48,7 +48,7 @@ struct stack_info {
 };
 
 extern int unwind_frame(struct task_struct *tsk, struct stackframe *frame);
-extern void walk_stackframe(struct task_struct *tsk, struct stackframe *frame,
+extern int walk_stackframe(struct task_struct *tsk, struct stackframe *frame,
 			    int (*fn)(struct stackframe *, void *), void *data);
 extern void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk);
 
