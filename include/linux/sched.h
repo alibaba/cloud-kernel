e7b88a8afaa422663613dc9dcf939f6889809362
Yafang Shao <laoar.shao@gmail.com>
2020-03-16 21:28:05 -0400

    psi: Move PF_MEMSTALL out of task->flags
    task #28327019
    
    commit 1066d1b6974e095d5a6c472ad9180a957b496cd6 upstream
    
    The task->flags is a 32-bits flag, in which 31 bits have already been
    consumed. So it is hardly to introduce other new per process flag.
    Currently there're still enough spaces in the bit-field section of
    task_struct, so we can define the memstall state as a single bit in
    task_struct instead.
    This patch also removes an out-of-date comment pointed by Matthew.
    
    Suggested-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Link: https://lkml.kernel.org/r/1584408485-1921-1-git-send-email-laoar.shao@gmail.com
    Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -762,8 +762,11 @@ struct task_struct {
 	unsigned			no_cgroup_migration:1;
 #endif
 #ifdef CONFIG_BLK_CGROUP
-	/* to be used once the psi infrastructure lands upstream. */
 	unsigned			use_memdelay:1;
+#endif
+#ifdef CONFIG_PSI
+       /* Stalled due to lack of memory */
+       unsigned                        in_memstall:1;
 #endif
 
 	unsigned long			atomic_flags; /* Flags requiring atomic access. */
@@ -1472,7 +1475,6 @@ extern struct pid *cad_pid;
 #define PF_KTHREAD		0x00200000	/* I am a kernel thread */
 #define PF_RANDOMIZE		0x00400000	/* Randomize virtual address space */
 #define PF_SWAPWRITE		0x00800000	/* Allowed to write to swap */
-#define PF_MEMSTALL		0x01000000	/* Stalled due to lack of memory */
 #define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
 #define PF_IO_WORKER		0x20000000	/* Task is an IO worker */
