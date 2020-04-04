19a4cd3c3a685e0c3819d98aa58e17e08ca526a0
Mel Gorman <mgorman@techsingularity.net>
2020-04-04 08:17:29 +0800

    mm, page_alloc: always use a captured page regardless of compaction result
    to #26255339
    
    commit ee8ab0eeb49bd3982090c8f14dc9cc65bcd13c5c upstream
    
    During the development of commit 5e1f0f098b46 ("mm, compaction: capture
    a page under direct compaction"), a paranoid check was added to ensure
    that if a captured page was available after compaction that it was
    consistent with the final state of compaction.  The intent was to catch
    serious programming bugs such as using a stale page pointer and causing
    corruption problems.
    
    However, it is possible to get a captured page even if compaction was
    unsuccessful if an interrupt triggered and happened to free pages in
    interrupt context that got merged into a suitable high-order page.  It's
    highly unlikely but Li Wang did report the following warning on s390
    occuring when testing OOM handling.  Note that the warning is slightly
    edited for clarity.
    
      WARNING: CPU: 0 PID: 9783 at mm/page_alloc.c:3777 __alloc_pages_direct_compact+0x182/0x190
      Modules linked in: rpcsec_gss_krb5 auth_rpcgss nfsv4 dns_resolver nfs
        lockd grace fscache sunrpc pkey ghash_s390 prng xts aes_s390
        des_s390 des_generic sha512_s390 zcrypt_cex4 zcrypt vmur binfmt_misc
        ip_tables xfs libcrc32c dasd_fba_mod qeth_l2 dasd_eckd_mod dasd_mod
        qeth qdio lcs ctcm ccwgroup fsm dm_mirror dm_region_hash dm_log
        dm_mod
      CPU: 0 PID: 9783 Comm: copy.sh Kdump: loaded Not tainted 5.1.0-rc 5 #1
    
    This patch simply removes the check entirely instead of trying to be
    clever about pages freed from interrupt context.  If a serious
    programming error was introduced, it is highly likely to be caught by
    prep_new_page() instead.
    
    Link: http://lkml.kernel.org/r/20190419085133.GH18914@techsingularity.net
    Fixes: 5e1f0f098b46 ("mm, compaction: capture a page under direct compaction")
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Reported-by: Li Wang <liwang@redhat.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Yang Shi <yang.shi@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3793,11 +3793,6 @@ __alloc_pages_direct_compact(gfp_t gfp_mask, unsigned int order,
 	memcg_lat_stat_end(MEM_LAT_DIRECT_COMPACT, start);
 	psi_memstall_leave(&pflags);
 
-	if (*compact_result <= COMPACT_INACTIVE) {
-		WARN_ON_ONCE(page);
-		return NULL;
-	}
-
 	/*
 	 * At least in one zone compaction wasn't deferred or skipped, so let's
 	 * count a compaction stall
