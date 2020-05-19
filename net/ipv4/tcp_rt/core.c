03fcbae2ecd73a8a4c70b1f3b4afbe36e4463fd9
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2020-05-19 11:52:37 +0800

    alinux: tcp_rt module: add tcp_rt module
    to #27804112
    
    TCP-RT is a kernel module for monitoring services at the tcp level.
    
    TCP-RT is essentially a trace method. By burying points in advance in the
    corresponding position in the kernel tcp protocol stack, we can identify
    the request and response from the scenario where there is only one concurrent
    request for a single connection, then collect the time when the request is
    received in the protocol stack and the time-consuming information on the
    processing of the service process and so on.
    
    In addition, TCP-RT also supports some statistical analysis in the kernel
    and periodically outputs statistical information about the specified connection.
    
    Signed-off-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Acked-by: Dust Li <dust.li@linux.alibaba.com>
    Reviewed-by: Ya Zhao <zhaoya123@linux.alibaba.com>
    Reviewed-by: Cambda Zhu <cambda@linux.alibaba.com>

---
+++ net/ipv4/tcp_rt/core.c
@@ -0,0 +1,377 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include "tcp_rt.h"
+
+static int            ports_number;
+static int            ports_peer_number;
+
+static int            ports[PORT_MAX_NUM];
+static int            ports_peer[PORT_MAX_NUM];
+
+static int            log_buf_num = 8;
+static int            real_buf_num = 2;
+
+static int            check = 1;
+static int            check_interval = 60;
+
+module_param(check, int, 0644);
+MODULE_PARM_DESC(check, "whether check the init_cwnd is ok");
+
+module_param(check_interval, int, 0644);
+MODULE_PARM_DESC(check_interval, "how many seconds later do stats");
+
+module_param_array(ports, int, &ports_number, 0644);
+MODULE_PARM_DESC(ports, "port array of	port_number size");
+
+module_param_array(ports_peer, int, &ports_peer_number, 0644);
+MODULE_PARM_DESC(ports_peer, "peer ports array");
+
+module_param(log_buf_num, int, 0644);
+MODULE_PARM_DESC(log_buf_num, "the num of buffers for every cpu log buffer. unit is 256k. just work when module load.");
+module_param(real_buf_num, int, 0644);
+MODULE_PARM_DESC(real_buf_num, "the num of buffers for every cpu real buffer. unit is 16k. just work when module load.");
+
+struct timer_list tcp_rt_timer;
+
+static void tcp_rt_timer_handler(struct timer_list *t)
+{
+	if (check) {
+		int i = 0;
+
+		for (i = 0; i < ports_number; i++)
+			tcp_rt_timer_output(i, ports[i], "L");
+
+		for (i = 0; i < ports_peer_number; i++)
+			tcp_rt_timer_output(i, ports_peer[i], "P");
+	}
+	mod_timer(&tcp_rt_timer, jiffies + check_interval * HZ);
+}
+
+static void tcp_rt_sk_send_data_peer(const struct sock *sk, struct tcp_rt *rt,
+				     const struct tcp_sock *tp)
+{
+	switch (rt->stage_peer) {
+	case TCPRT_STAGE_PEER_RESPONSE:
+		tcp_rt_log_printk(sk, LOG_STATUS_P, false, check);
+		/* fall through */
+
+	case TCPRT_STAGE_PEER_NONE:
+		ktime_get_real_ts64(&rt->start_time);
+		rt->request_num += 1;
+		rt->upload_data = 0;
+		rt->stage_peer = TCPRT_STAGE_PEER_REQUEST;
+
+		rt->last_total_retrans = tp->total_retrans;
+		break;
+
+	default:
+		break;
+	}
+}
+
+static void tcp_rt_sk_send_data_local(const struct sock *sk, struct tcp_rt *rt,
+				      const struct tcp_sock *tp)
+{
+	u64 this_us;
+
+	switch (rt->stage) {
+	case TCPRT_STAGE_REQUEST:
+		this_us = tcp_clock_us();
+
+		rt->stage = TCPRT_STAGE_RESPONSE;
+
+		rt->server_time = this_us - rt->lrcvtime_us;
+		rt->upload_time = rt->lrcvtime_us - rt->frcvtime_us;
+		rt->upload_data = tp->rcv_nxt - rt->start_rcv_nxt;
+
+		/* because recv_data is after rcv_nxt update, so
+		 * record the value at here.
+		 */
+		rt->start_rcv_nxt = tp->rcv_nxt;
+
+		break;
+
+	case TCPRT_STAGE_RESPONSE:
+		break;
+
+	case TCPRT_STAGE_NONE:
+		break;
+	}
+}
+
+static void tcp_rt_sk_recv_data_peer(const struct sock *sk, struct tcp_rt *rt,
+				     const struct tcp_sock *tp)
+{
+	switch (rt->stage_peer) {
+	case TCPRT_STAGE_PEER_REQUEST:
+		rt->stage_peer = TCPRT_STAGE_PEER_RESPONSE;
+		rt->upload_data = tp->snd_nxt - rt->start_seq;
+		rt->start_seq = tp->snd_nxt;
+		return;
+
+	default:
+		break;
+	}
+}
+
+static void tcp_rt_sk_recv_data_local(const struct sock *sk, struct tcp_rt *rt,
+				      const struct tcp_sock *tp)
+{
+	switch (rt->stage) {
+	case TCPRT_STAGE_RESPONSE:
+		if (after(tp->snd_nxt, tp->snd_una + 1)) {
+			/* there are some bytes not acked.
+			 * but new request is coming,
+			 * so the skb also be the acked skb for snd_una,
+			 * so update the end_time
+			 */
+			ktime_get_real_ts64(&rt->end_time);
+		}
+		tcp_rt_log_printk(sk, LOG_STATUS_R, false, check);
+
+		/* fall through */
+
+	case TCPRT_STAGE_NONE:
+		ktime_get_real_ts64(&rt->start_time);
+		rt->end_time = rt->start_time;
+
+		rt->frcvtime_us = tcp_clock_us();
+		rt->lrcvtime_us = rt->frcvtime_us;
+
+		rt->start_seq =	 tp->snd_nxt;
+
+		rt->last_total_retrans = tp->total_retrans;
+		rt->last_update_seq = tp->snd_una;
+
+		rt->server_time = 0;
+		rt->upload_time = 0;
+		rt->upload_data = 0;
+
+		rt->rcv_reorder = 0;
+
+		rt->stage = TCPRT_STAGE_REQUEST;
+
+		rt->request_num += 1;
+		if (!RB_EMPTY_ROOT(&tp->out_of_order_queue))
+			rt->rcv_reorder = 1;
+		break;
+
+	case TCPRT_STAGE_REQUEST:
+		rt->lrcvtime_us = tcp_clock_us();
+		if (!RB_EMPTY_ROOT(&tp->out_of_order_queue))
+			rt->rcv_reorder = 1;
+		break;
+	}
+}
+
+static void tcp_rt_sk_release_peer(const struct sock *sk, struct tcp_rt *rt,
+				   const struct tcp_sock *tp)
+{
+	switch (rt->stage_peer) {
+	case TCPRT_STAGE_PEER_NONE:
+	case TCPRT_STAGE_PEER_REQUEST:
+		return;
+
+	case TCPRT_STAGE_PEER_RESPONSE:
+		tcp_rt_log_printk(sk, LOG_STATUS_P, false, check);
+		return;
+	}
+}
+
+static void tcp_rt_sk_release_local(const struct sock *sk, struct tcp_rt *rt,
+				    const struct tcp_sock *tp)
+{
+	switch (rt->stage) {
+	case TCPRT_TYPE_NONE:
+		break;
+
+	case TCPRT_STAGE_REQUEST:
+		/* closed when receiving data */
+		tcp_rt_log_printk(sk, LOG_STATUS_N, false, check);
+		break;
+
+	case TCPRT_STAGE_RESPONSE:
+		if (tp->snd_nxt <= rt->start_seq + 1)
+			break;
+
+		if (tp->snd_nxt <= tp->snd_una + 1) {
+			if (after(tp->snd_una, rt->last_update_seq + 1))
+				ktime_get_real_ts64(&rt->end_time);
+
+			tcp_rt_log_printk(sk, LOG_STATUS_R, true,
+					  check);
+		} else {
+			/* closed when sending data */
+			tcp_rt_log_printk(sk, LOG_STATUS_W, false,
+					  check);
+		}
+
+		break;
+	}
+}
+
+static void tcp_rt_sk_release(struct sock *sk)
+{
+	const struct tcp_sock *tp = tcp_sk(sk);
+	struct tcp_rt *rt = TCP_SK_RT(sk);
+
+	if (!rt->request_num)
+		goto free;
+
+	if (rt->type == TCPRT_TYPE_PEER_PORT)
+		tcp_rt_sk_release_peer(sk, rt, tp);
+	else
+		tcp_rt_sk_release_local(sk, rt, tp);
+
+	/* closed, 1 record per connection */
+	tcp_rt_log_printk(sk, LOG_STATUS_E, false, check);
+
+free:
+	kfree(rt);
+}
+
+static void tcp_rt_sk_pkts_acked(struct sock *sk)
+{
+	struct tcp_sock *tp = tcp_sk(sk);
+	struct tcp_rt *rt = TCP_SK_RT(sk);
+
+	if (after(tp->snd_una, rt->last_update_seq + 1)) {
+		ktime_get_real_ts64(&rt->end_time);
+		rt->last_update_seq = tp->snd_una;
+	}
+}
+
+static void tcp_rt_sk_send_data(struct sock *sk)
+{
+	struct tcp_sock *tp = tcp_sk(sk);
+	struct tcp_rt *rt = TCP_SK_RT(sk);
+
+	if (rt->type == TCPRT_TYPE_PEER_PORT)
+		return tcp_rt_sk_send_data_peer(sk, rt, tp);
+	else
+		return tcp_rt_sk_send_data_local(sk, rt, tp);
+}
+
+static void tcp_rt_sk_recv_data(struct sock *sk)
+{
+	struct tcp_rt *rt = TCP_SK_RT(sk);
+	struct tcp_sock *tp = tcp_sk(sk);
+
+	if (rt->type == TCPRT_TYPE_PEER_PORT)
+		return tcp_rt_sk_recv_data_peer(sk, rt, tp);
+	else
+		return tcp_rt_sk_recv_data_local(sk, rt, tp);
+}
+
+static int tcp_rt_sk_init(struct sock *sk)
+{
+	struct tcp_sock *tp = tcp_sk(sk);
+	struct inet_connection_sock *icsk = inet_csk(sk);
+
+	struct tcp_rt *rt = NULL;
+	enum tcp_rt_type type = TCPRT_TYPE_NONE;
+	int i, port;
+
+	port = ntohs(inet_sk(sk)->inet_sport);
+
+	for (i = 0; i < ports_number; i++) {
+		if (port == ports[i]) {
+			type = TCPRT_TYPE_LOCAL_PORT;
+			goto ok;
+		}
+	}
+
+	port = ntohs(inet_sk(sk)->inet_dport);
+
+	for (i = 0; i < ports_peer_number; i++) {
+		if (port == ports_peer[i]) {
+			type = TCPRT_TYPE_PEER_PORT;
+			goto ok;
+		}
+	}
+
+	return -1;
+ok:
+	rt = kmalloc(sizeof(*rt), GFP_KERNEL);
+	if (!rt)
+		return -1;
+
+	memset(rt, 0, sizeof(*rt));
+
+	icsk->icsk_tcp_rt_priv = (void *)rt;
+
+	rt->type  = type;
+	rt->index = i;
+
+	rt->con_start_seq = tp->snd_nxt;
+	rt->con_rcv_nxt = tp->rcv_nxt;
+
+	/* because recv_data is after rcv_nxt update, so
+	 * record the value at here.
+	 */
+	rt->start_rcv_nxt = tp->rcv_nxt;
+	rt->start_seq = tp->snd_nxt;
+	ktime_get_real_ts64(&rt->con_start_time);
+	return 0;
+}
+
+static struct tcp_rt_ops rt_ops __read_mostly = {
+	.owner      = THIS_MODULE,
+	.init       = tcp_rt_sk_init,
+	.recv_data  = tcp_rt_sk_recv_data,
+	.send_data  = tcp_rt_sk_send_data,
+	.pkts_acked = tcp_rt_sk_pkts_acked,
+	.release    = tcp_rt_sk_release,
+};
+
+static ssize_t tcp_rt_deactivate(struct file *file, const char __user *buff,
+				 size_t count, loff_t *offset)
+{
+	pr_info("tcp-rt: deactivate\n");
+	tcp_unregister_rt(&rt_ops);
+	return count;
+}
+
+static const struct file_operations fops = {
+	.owner      = THIS_MODULE,
+	.write      = tcp_rt_deactivate,
+};
+
+static int __init tcp_rt_module_init(void)
+{
+	int ret;
+
+	ret = tcp_rt_output_init(log_buf_num, real_buf_num, &fops);
+	if (ret)
+		return ret;
+
+	ret = tcp_register_rt(&rt_ops);
+	if (ret) {
+		pr_err("tcp-rt register rt failed!\n");
+		tcp_rt_output_released();
+		return ret;
+	}
+
+	timer_setup(&tcp_rt_timer, tcp_rt_timer_handler, 0);
+	tcp_rt_timer.expires = jiffies + 10 * HZ;
+	add_timer(&tcp_rt_timer);
+
+	pr_info("tcp-rt: module load success\n");
+	return 0;
+}
+
+static void __exit tcp_rt_module_fini(void)
+{
+	del_timer_sync(&tcp_rt_timer);
+	tcp_rt_output_released();
+	pr_info("tcp-rt: module unloaded\n");
+}
+
+module_init(tcp_rt_module_init);
+module_exit(tcp_rt_module_fini);
+
+MODULE_AUTHOR("xuanzhuo <xuanzhuo@linux.alibaba.com>");
+MODULE_AUTHOR("Cambda Zhu <cambda@linux.alibaba.com>");
+MODULE_AUTHOR("Ya Zhao <zhaoya123@linux.alibaba.com>");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("TCP RT");
---
+++ net/ipv4/tcp_rt/tcp_rt.h
@@ -0,0 +1,115 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2018 Alibaba Group
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#include <linux/mm.h>
+#include <linux/debugfs.h>
+#include <linux/timer.h>
+#include <linux/percpu.h>
+#include <linux/version.h>
+#include <linux/relay.h>
+#include <linux/module.h>
+#include <net/tcp.h>
+
+#define TCP_SK_RT(sk)  (inet_csk(sk)->icsk_tcp_rt_priv)
+
+#define LOG_SUBBUF_SIZE   262144
+#define STATS_SUBBUF_SIZE 16384
+#define PORT_MAX_NUM      6
+
+#define LOG_STATUS_R 'R'
+#define LOG_STATUS_W 'W'
+#define LOG_STATUS_N 'N'
+#define LOG_STATUS_E 'E'
+#define LOG_STATUS_P 'P'
+
+enum tcp_rt_type {
+	TCPRT_TYPE_NONE,
+	TCPRT_TYPE_LOCAL_PORT,
+	TCPRT_TYPE_LOCAL_PORT_RANG,
+	TCPRT_TYPE_PEER_PORT,
+};
+
+enum tcp_rt_stage {
+	TCPRT_STAGE_NONE,
+	TCPRT_STAGE_REQUEST,
+	TCPRT_STAGE_RESPONSE,
+};
+
+enum tcp_rt_stage_peer {
+	TCPRT_STAGE_PEER_NONE,
+	TCPRT_STAGE_PEER_REQUEST,
+	TCPRT_STAGE_PEER_RESPONSE,
+};
+
+struct tcp_rt {
+	enum tcp_rt_type  type;
+	struct timespec64 con_start_time;
+
+	u32              con_start_seq;
+	u32              con_rcv_nxt;
+	u32              index;
+	u32              request_num;
+
+	/* task item */
+	struct timespec64    start_time;
+	struct timespec64    end_time;
+
+	u64               frcvtime_us;
+	u64               lrcvtime_us;
+
+	u32               start_seq;
+	u32               start_rcv_nxt;
+
+	u32               last_total_retrans;
+	u32               last_update_seq;
+
+	int               server_time;
+	int               upload_time;
+	u32               upload_data;
+
+	u8                rcv_reorder;
+
+	enum tcp_rt_stage stage;
+	enum tcp_rt_stage_peer stage_peer;
+};
+
+struct tcp_rt_real {
+	atomic64_t rt;
+	atomic64_t number;
+	atomic64_t drop;
+	atomic64_t bytes;
+	atomic64_t server_time;
+	atomic64_t fail;
+	atomic64_t packets;
+	atomic64_t rtt;
+	atomic64_t upload_time;
+	atomic64_t upload_data;
+	atomic64_t con_num;
+};
+
+struct _tcp_rt_real {
+	u32 rt;
+	u32 number;
+	u32 drop;
+	u32 bytes;
+	u32 server_time;
+	u32 fail;
+	u32 packets;
+	u32 rtt;
+	u32 upload_time;
+	u32 upload_data;
+	u32 con_num;
+};
+
+int tcp_rt_output_init(int log_buf_num, int real_buf_num,
+		       const struct file_operations *fops);
+void tcp_rt_output_released(void);
+void tcp_rt_log_printk(const struct sock *sk, char flag, bool fin, bool check);
+void tcp_rt_timer_output(int index, int port, char *flag);
