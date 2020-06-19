967e3f17fa6cd4adf40ff1476f35d61f544aef68
Tuan Phan <tphan@amperecomputing.com>
2020-06-19 11:50:22 -0700

    amp: drivers/pci: Add ecam quirk for Ampere Altra SOC.
    fix #29194885
    
    commit 0d8dd94ec5d2d4aee8a955763ed93a49187b6c77 ampere
    
    Ampere Altra SOC only supports 32bit PCI ecam read. Therefore,
    add an ecam quirk for the platform.
    
    Signed-off-by: Tuan Phan <tphan@amperecomputing.com>
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Acked-by: Alex Shi <alex.shi@linux.alibaba.com>
    Reviewed-by: luanshi <zhangliguang@linux.alibaba.com>

--- include/linux/pci-ecam.h
+++ include/linux/pci-ecam.h
@@ -56,6 +56,7 @@ extern struct pci_ecam_ops thunder_pem_ecam_ops; /* Cavium ThunderX 1.x & 2.x */
 extern struct pci_ecam_ops pci_thunder_ecam_ops; /* Cavium ThunderX 1.x */
 extern struct pci_ecam_ops xgene_v1_pcie_ecam_ops; /* APM X-Gene PCIe v1 */
 extern struct pci_ecam_ops xgene_v2_pcie_ecam_ops; /* APM X-Gene PCIe v2.x */
+extern struct pci_ecam_ops pci_32b_read_ops;   /* 32-bit read accesses only */
 #endif
 
 #ifdef CONFIG_PCI_HOST_COMMON
