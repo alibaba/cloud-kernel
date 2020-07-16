b997fd70f1e7cdaddc33e47f0ba221dc7a5b0d8b
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2020-07-16 17:52:45 +0800

    Intel: cpuidle: Allow idle states to be disabled by default
    task #29239886
    
    commit 75a80267410e38ab76c4ceb39753f96d72113781 upstream
    
    In certain situations it may be useful to prevent some idle states
    from being used by default while allowing user space to enable them
    later on.
    
    For this purpose, introduce a new state flag, CPUIDLE_FLAG_OFF, to
    mark idle states that should be disabled by default, make the core
    set CPUIDLE_STATE_DISABLED_BY_USER for those states at the
    initialization time and add a new state attribute in sysfs,
    "default_status", to inform user space of the initial status of
    the given idle state ("disabled" if CPUIDLE_FLAG_OFF is set for it,
    "enabled" otherwise).
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: yjia <yingbao.jia@intel.com>
    Signed-off-by: Erwei Deng <erwei@linux.alibaba.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- drivers/cpuidle/sysfs.c
+++ drivers/cpuidle/sysfs.c
@@ -292,6 +292,14 @@ static ssize_t show_state_##_name(struct cpuidle_state *state, \
 	return sprintf(buf, "%s\n", state->_name);\
 }
 
+static ssize_t show_state_default_status(struct cpuidle_state *state,
+					struct cpuidle_state_usage *state_usage,
+					char *buf)
+{
+	return sprintf(buf, "%s\n",
+		state->flags & CPUIDLE_FLAG_OFF ? "disabled" : "enabled");
+}
+
 define_show_state_function(exit_latency)
 define_show_state_function(target_residency)
 define_show_state_function(power_usage)
@@ -310,6 +318,7 @@ define_one_state_ro(power, show_state_power_usage);
 define_one_state_ro(usage, show_state_usage);
 define_one_state_ro(time, show_state_time);
 define_one_state_rw(disable, show_state_disable, store_state_disable);
+define_one_state_ro(default_status, show_state_default_status);
 
 static struct attribute *cpuidle_state_default_attrs[] = {
 	&attr_name.attr,
@@ -320,6 +329,7 @@ static struct attribute *cpuidle_state_default_attrs[] = {
 	&attr_usage.attr,
 	&attr_time.attr,
 	&attr_disable.attr,
+	&attr_default_status.attr,
 	NULL
 };
 
