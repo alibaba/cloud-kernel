e598bc81810abc125d178f42d73bb2947d76dc06
Thomas Gleixner <tglx@linutronix.de>
2020-07-27 11:11:25 -0400

    genirq: Add protection against unsafe usage of generic_handle_irq()
    task #29600094
    
    commit c16816acd08697b02a53f56f8936497a9f6f6e7a upstream.
    Backport summary: for 4.19 kernel ICX PCIe Gen4 support.
    
    In general calling generic_handle_irq() with interrupts disabled from non
    interrupt context is harmless. For some interrupt controllers like the x86
    trainwrecks this is outright dangerous as it might corrupt state if an
    interrupt affinity change is pending.
    
    Add infrastructure which allows to mark interrupts as unsafe and catch such
    usage in generic_handle_irq().
    
    Reported-by: sathyanarayanan.kuppuswamy@linux.intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Link: https://lkml.kernel.org/r/20200306130623.590923677@linutronix.de
    
    (cherry picked from commit c16816acd08697b02a53f56f8936497a9f6f6e7a)
    Signed-off-by: Ethan Zhao <haifeng.zhao@intel.com>
    
    Conflicts:
    	include/linux/irq.h
    
    Signed-off-by: Artie Ding <artie.ding@linux.alibaba.com>
    Acked-by: Caspar Zhang <caspar@linux.alibaba.com>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -208,6 +208,10 @@ struct irq_data {
  * IRQD_SINGLE_TARGET		- IRQ allows only a single affinity target
  * IRQD_DEFAULT_TRIGGER_SET	- Expected trigger already been set
  * IRQD_CAN_RESERVE		- Can use reservation mode
+ * IRQD_MSI_NOMASK_QUIRK	- Non-maskable MSI quirk for affinity change
+ *				  required
+ * IRQD_HANDLE_ENFORCE_IRQCTX	- Enforce that handle_irq_*() is only invoked
+ *				  from actual interrupt context.
  */
 enum {
 	IRQD_TRIGGER_MASK		= 0xf,
@@ -230,6 +234,8 @@ enum {
 	IRQD_SINGLE_TARGET		= (1 << 24),
 	IRQD_DEFAULT_TRIGGER_SET	= (1 << 25),
 	IRQD_CAN_RESERVE		= (1 << 26),
+	IRQD_MSI_NOMASK_QUIRK		= (1 << 27),
+	IRQD_HANDLE_ENFORCE_IRQCTX	= (1 << 28),
 };
 
 #define __irqd_to_state(d) ACCESS_PRIVATE((d)->common, state_use_accessors)
@@ -297,6 +303,16 @@ static inline void irqd_set_single_target(struct irq_data *d)
 static inline bool irqd_is_single_target(struct irq_data *d)
 {
 	return __irqd_to_state(d) & IRQD_SINGLE_TARGET;
+}
+
+static inline void irqd_set_handle_enforce_irqctx(struct irq_data *d)
+{
+	__irqd_to_state(d) |= IRQD_HANDLE_ENFORCE_IRQCTX;
+}
+
+static inline bool irqd_is_handle_enforce_irqctx(struct irq_data *d)
+{
+	return __irqd_to_state(d) & IRQD_HANDLE_ENFORCE_IRQCTX;
 }
 
 static inline bool irqd_is_wakeup_set(struct irq_data *d)
