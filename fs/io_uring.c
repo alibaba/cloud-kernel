ae1d80b454c8de39da574a36492b0d9160e6849d
Jens Axboe <axboe@kernel.dk>
2020-08-24 11:45:26 -0600

    io_uring: revert consumed iov_iter bytes on error
    ANBZ: #190
    
    commit 842163154b87b01d8f516af15ad8916eb1661016 upstream.
    
    Some consumers of the iov_iter will return an error, but still have
    bytes consumed in the iterator. This is an issue for -EAGAIN, since we
    rely on a sane iov_iter state across retries.
    
    Fix this by ensuring that we revert consumed bytes, if any, if the file
    operations have consumed any bytes from iterator. This is similar to what
    generic_file_read_iter() does, and is always safe as we have the previous
    bytes count handy already.
    
    Fixes: ff6165b2d7f6 ("io_uring: retain iov_iter state over io_read/io_write calls")
    Reported-by: Dmitry Shulyak <yashulyak@gmail.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Reviewed-by: Hao Xu <haoxu@linux.alibaba.com>
    Tested-by: Hao Xu <haoxu@linux.alibaba.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -3096,6 +3096,8 @@ static int io_read(struct io_kiocb *req, bool force_nonblock)
 		ret = 0;
 		goto out_free;
 	} else if (ret == -EAGAIN) {
+		/* some cases will consume bytes even on error returns */
+		iov_iter_revert(iter, iov_count - iov_iter_count(iter));
 		ret = io_setup_async_rw(req, iovec, inline_vecs, iter, false);
 		if (ret)
 			goto out_free;
@@ -3244,6 +3246,8 @@ static int io_write(struct io_kiocb *req, bool force_nonblock)
 	if (!force_nonblock || ret2 != -EAGAIN) {
 		kiocb_done(kiocb, ret2);
 	} else {
+		/* some cases will consume bytes even on error returns */
+		iov_iter_revert(iter, iov_count - iov_iter_count(iter));
 copy_iov:
 		ret = io_setup_async_rw(req, iovec, inline_vecs, iter, false);
 		if (!ret)
