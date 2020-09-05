a27d73e707fd586ac86a28cb6e609011588b1c70
Jens Axboe <axboe@kernel.dk>
2020-09-04 22:36:52 -0600

    io_uring: fix explicit async read/write mapping for large segments
    ANBZ: #190
    
    commit c183edff33fdcd639d222a8f473bf44602adc655 upstream.
    
    If we exceed UIO_FASTIOV, we don't handle the transition correctly
    between an allocated vec for requests that are queued with IOSQE_ASYNC.
    Store the iovec appropriately and re-set it in the iter iov in case
    it changed.
    
    Fixes: ff6165b2d7f6 ("io_uring: retain iov_iter state over io_read/io_write calls")
    Reported-by: Nick Hill <nick@nickhill.org>
    Tested-by: Norman Maurer <norman.maurer@googlemail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Reviewed-by: Hao Xu <haoxu@linux.alibaba.com>
    Tested-by: Hao Xu <haoxu@linux.alibaba.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2901,17 +2901,18 @@ static inline int io_rw_prep_async(struct io_kiocb *req, int rw,
 				   bool force_nonblock)
 {
 	struct io_async_rw *iorw = &req->io->rw;
+	struct iovec *iov;
 	ssize_t ret;
 
-	iorw->iter.iov = iorw->fast_iov;
+	iorw->iter.iov = iov = iorw->fast_iov;
 	/* reset ->io around the iovec import, we don't want to use it */
 	req->io = NULL;
-	ret = io_import_iovec(rw, req, (struct iovec **) &iorw->iter.iov,
-				&iorw->iter, !force_nonblock);
+	ret = io_import_iovec(rw, req, &iov, &iorw->iter, !force_nonblock);
 	req->io = container_of(iorw, struct io_async_ctx, rw);
 	if (unlikely(ret < 0))
 		return ret;
 
+	iorw->iter.iov = iov;
 	io_req_map_rw(req, iorw->iter.iov, iorw->fast_iov, &iorw->iter);
 	return 0;
 }
