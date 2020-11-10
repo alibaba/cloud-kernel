91559bea78494c7b4e16e60c30b77f7d3ea1da51
Jeffle Xu <jefflexu@linux.alibaba.com>
2020-11-10 16:14:13 +0800

    alinux: Revert "blk-mq: fix NULL pointer deference in case no poll implementation"
    This reverts commit 1e41c5056619ec50b8146534881d6e0d6a7ca5ba.
    
    Back upon the time of this commit, QUEUE_FLAG_POLL is included in
    default MQ flags. QUEUE_FLAG_POLL will be set for every mq device even
    the mq_ops->poll may be NULL, and thus may trigger NULL pointer in
    blk_poll().
    
    commit 294d5fb28de8 ("alinux: blk-mq: remove QUEUE_FLAG_POLL from
    default MQ flags") has removed QUEUE_FLAG_POLL from default MQ flags,
    and set QUEUE_FLAG_POLL only when mq_ops->poll is non-NULL.
    
    Since then the original commit is no longer needed.
    
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -3450,7 +3450,7 @@ int blk_poll(struct request_queue *q, blk_qc_t cookie, bool spin)
 	struct blk_mq_hw_ctx *hctx;
 	long state;
 
-	if (!blk_qc_t_valid(cookie) || !q->mq_ops->poll ||
+	if (!blk_qc_t_valid(cookie) ||
 	    !test_bit(QUEUE_FLAG_POLL, &q->queue_flags))
 		return 0;
 
