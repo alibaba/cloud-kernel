21b065edfa2a990c5f4a9474e74e2d0991b06dfa
Chang S. Bae <chang.seok.bae@intel.com>
2021-02-21 10:56:32 -0800

    Intel: x86/cpufeatures/amx: Enumerate Advanced Matrix Extension (AMX) feature bits
    to #35358497
    
    commit: https://lore.kernel.org/patchwork/patch/1382814/
    
    Intel's Advanced Matrix Extension (AMX) is a new 64-bit extended feature
    consisting of two-dimensional registers and an accelerator unit. The first
    implementation of the latter is the tile matrix multiply unit (TMUL). TMUL
    performs SIMD dot-products on four bytes (INT8) or two bfloat16
    floating-point (BF16) elements.
    
    Here we add AMX to the kernel/user ABI, by enumerating the capability.
    E.g., /proc/cpuinfo: amx_tile, amx_bf16, amx_int8
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Reviewed-by: Len Brown <len.brown@intel.com>
    Cc: x86@kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Lin Wang <lin.x.wang@intel.com>
    Signed-off-by: Guanjun <guanjun@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Acked-by: Artie Ding <artie.ding@linux.alibaba.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -379,6 +379,9 @@
 #define X86_FEATURE_PCONFIG		(18*32+18) /* Intel PCONFIG */
 #define X86_FEATURE_ARCH_LBR		(18*32+19) /* Intel ARCH LBR */
 #define X86_FEATURE_AVX512_FP16		(18*32+23) /* AVX512 FP16 */
+#define X86_FEATURE_AMX_BF16		(18*32+22) /* AMX BF16 Support */
+#define X86_FEATURE_AMX_TILE		(18*32+24) /* AMX tile Support */
+#define X86_FEATURE_AMX_INT8		(18*32+25) /* AMX INT8 Support */
 #define X86_FEATURE_SPEC_CTRL		(18*32+26) /* "" Speculation Control (IBRS + IBPB) */
 #define X86_FEATURE_INTEL_STIBP		(18*32+27) /* "" Single Thread Indirect Branch Predictors */
 #define X86_FEATURE_FLUSH_L1D		(18*32+28) /* Flush L1D cache */
