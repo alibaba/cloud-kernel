8ae8220021636fbe24f05523824c2ebb9f96aea7
Xie XiuQi <xiexiuqi@huawei.com>
2021-02-26 20:21:40 +0800

    openEuler: arm64/mpam: cleanup the source file's licence
    to #34407882
    
    commit 63e61f491e5957fdb198dda97944e76c35c22e34 openEuler
    
    hulk inclusion
    category: bugfix
    bugzilla: 48265
    CVE: NA
    
    --------------------------------
    
    Rewrite the source file's licence of mpam feature.
    
    Signed-off-by: Xie XiuQi <xiexiuqi@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Wang ShaoBo <bobo.shaobowang@huawei.com>
    Reviewed-by: Cheng Jian <cj.chengjian@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>

--- fs/resctrlfs.c
+++ fs/resctrlfs.c
@@ -1,9 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * User interface for Resource Alloction in Resource Director Technology(RDT)
+ * User interface for ARM v8 MPAM
  *
- * Copyright (C) 2016 Intel Corporation
+ * Copyright (C) 2018-2019 Huawei Technologies Co., Ltd
  *
- * Author: Fenghua Yu <fenghua.yu@intel.com>
+ * Author: Xie XiuQi <xiexiuqi@huawei.com>
+ *
+ * Code was partially borrowed from arch/x86/kernel/cpu/intel_rdt*.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -14,8 +17,10 @@
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
- * More information about RDT be found in the Intel (R) x86 Architecture
- * Software Developer Manual.
+ * More information about MPAM be found in the Arm Architecture Reference
+ * Manual.
+ *
+ * https://static.docs.arm.com/ddi0598/a/DDI0598_MPAM_supp_armv8a.pdf
  */
 
 #define pr_fmt(fmt)	KBUILD_MODNAME ": " fmt
--- include/linux/resctrlfs.h
+++ include/linux/resctrlfs.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _RESCTRLFS_H
 #define _RESCTRLFS_H
 
