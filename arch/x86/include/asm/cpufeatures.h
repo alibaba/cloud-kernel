c7a686c284ec3e121b71150bc06ad75a939f3fee
Jarkko Sakkinen <jarkko@kernel.org>
2021-03-10 21:42:09 +0800

    x86/{cpufeatures,msr}: Add Intel SGX Launch Control hardware bits
    to #32051961
    commit d205e0f1426e0f99e2b4f387c49f2d8b66e129dd upstream.
    
    The SGX Launch Control hardware helps restrict which enclaves the
    hardware will run.  Launch control is intended to restrict what software
    can run with enclave protections, which helps protect the overall system
    from bad enclaves.
    
    For the kernel's purposes, there are effectively two modes in which the
    launch control hardware can operate: rigid and flexible. In its rigid
    mode, an entity other than the kernel has ultimate authority over which
    enclaves can be run (firmware, Intel, etc...). In its flexible mode, the
    kernel has ultimate authority over which enclaves can run.
    
    Enable X86_FEATURE_SGX_LC to enumerate when the CPU supports SGX Launch
    Control in general.
    
    Add MSR_IA32_SGXLEPUBKEYHASH{0, 1, 2, 3}, which when combined contain a
    SHA256 hash of a 3072-bit RSA public key. The hardware allows SGX enclaves
    signed with this public key to initialize and run [*]. Enclaves not signed
    with this key can not initialize and run.
    
    Add FEAT_CTL_SGX_LC_ENABLED, which informs whether the SGXLEPUBKEYHASH MSRs
    can be written by the kernel.
    
    If the MSRs do not exist or are read-only, the launch control hardware is
    operating in rigid mode. Linux does not and will not support creating
    enclaves when hardware is configured in rigid mode because it takes away
    the authority for launch decisions from the kernel. Note, this does not
    preclude KVM from virtualizing/exposing SGX to a KVM guest when launch
    control hardware is operating in rigid mode.
    
    [*] Intel SDM: 38.1.4 Intel SGX Launch Control Configuration
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Co-developed-by: Jarkko Sakkinen <jarkko@kernel.org>
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Jethro Beekman <jethro@fortanix.com>
    Link: https://lkml.kernel.org/r/20201112220135.165028-5-jarkko@kernel.org
    [GuoRui.Yu: Fix conflicts in arch/x86/include/asm/{cpufeatures.h,
    msr-index.h}]
    Signed-off-by: GuoRui.Yu <Guorui.Yu@linux.alibaba.com>
    Reviewed-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -344,6 +344,7 @@
 #define X86_FEATURE_LA57		(16*32+16) /* 5-level page tables */
 #define X86_FEATURE_RDPID		(16*32+22) /* RDPID instruction */
 #define X86_FEATURE_CLDEMOTE		(16*32+25) /* CLDEMOTE instruction */
+#define X86_FEATURE_SGX_LC		(16*32+30) /* Software Guard Extensions Launch Control */
 
 /* AMD-defined CPU features, CPUID level 0x80000007 (EBX), word 17 */
 #define X86_FEATURE_OVERFLOW_RECOV	(17*32+ 0) /* MCA overflow recovery support */
