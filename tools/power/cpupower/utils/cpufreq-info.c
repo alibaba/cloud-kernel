63c9b5bd508d01a80a03e315b51c03064b5ae0d2
Nathan Fontenot <nathan.fontenot@amd.com>
2021-04-12 09:47:03 +0800

    cpupower: Remove family arg to decode_pstates()
    to #33635513
    
    commit d1abc4e996d7784ce4d56749e4b5ca8ff23b1e0f upstream
    
    The decode_pstates() routine no longer uses the CPU family and
    the caleed routines (get_cof() and get_did()) can grab the family
    from the global cpupower_cpu_info struct. These update removes
    passing the family arg to all these routines.
    
    Signed-off-by: Nathan Fontenot <nathan.fontenot@amd.com>
    Reviewed-by: Robert Richter <rrichter@amd.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- tools/power/cpupower/utils/cpufreq-info.c
+++ tools/power/cpupower/utils/cpufreq-info.c
@@ -194,8 +194,7 @@ static int get_boost_mode(unsigned int cpu)
 	if ((cpupower_cpu_info.vendor == X86_VENDOR_AMD &&
 	     cpupower_cpu_info.family >= 0x10) ||
 	     cpupower_cpu_info.vendor == X86_VENDOR_HYGON) {
-		ret = decode_pstates(cpu, cpupower_cpu_info.family, b_states,
-				     pstates, &pstate_no);
+		ret = decode_pstates(cpu, b_states, pstates, &pstate_no);
 		if (ret)
 			return ret;
 
