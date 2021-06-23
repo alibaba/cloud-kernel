alibaba/cloud-kernel

commit 8711d908e6939209e6f7e8bd0eee077223735854

alinux: sched: make up nr_high/under_running for cfs bandwidth
fix #34030593

After we introduce nr_running make up mechanism, we still find out
that nr_high/under_running is incorrect in the throttle scenario.
In order to fix this problem, we improve the nr_running make up
mechanism in this patch. We record the delta of tasks when throttle/
unthrottle, and if se is NULL, we commit the delta to
nr_high/under_running.

Signed-off-by: Michael Wang <yun.wang@linux.alibaba.com>
Signed-off-by:Cruz Zhao <CruzZhao@linux.alibaba.com>
Acked-by: Michael Wang <yun.wang@linux.alibaba.com>

kernel/sched/fair.c
@@ -960,28 +960,27 @@ id_idle_cpu(struct task_struct *p, int cpu, bool expellee, bool *idle)
 }
 
 static noinline void
-id_make_up_nr_running(struct task_group *tg, struct rq *rq, long delta)
+id_update_make_up(struct task_group *tg, struct rq *rq, long delta)
 {
 	struct sched_entity *se = tg->se[cpu_of(rq)];
 
-	/*
-	 * The only case we should skip make up is
-	 * when enqueue_entity() triggered throttle.
-	 *
-	 * At that point the new arrived task has
-	 * not yet been accounted into rq, and will
-	 * not until unthrottled, so just skip the
-	 * make up and let unthrottle process do the
-	 * job.
-	 */
-	if (rq->skip_make_up || !delta)
-		return;
-
 	if (is_highclass(se))
-		rq->nr_high_running += delta;
+		rq->nr_high_make_up += delta;
 
 	if (is_underclass(se))
-		rq->nr_under_running += delta;
+		rq->nr_under_make_up += delta;
+}
+
+static noinline void
+id_commit_make_up(struct rq *rq, bool commit)
+{
+	if (commit) {
+		rq->nr_high_running += rq->nr_high_make_up;
+		rq->nr_under_running += rq->nr_under_make_up;
+	}
+
+	rq->nr_high_make_up = 0;
+	rq->nr_under_make_up = 0;
 }
 
 static __always_inline void
@@ -1764,7 +1763,12 @@ id_preempt_all(struct sched_entity *curr, struct sched_entity *se)
 }
 
 static inline void
-id_make_up_nr_running(struct task_group *tg, struct rq *rq, long delta)
+id_update_make_up(struct task_group *tg, struct rq *rq, long delta)
+{
+}
+
+static noinline void
+id_commit_make_up(struct rq *rq, bool commit)
 {
 }
 
@@ -5857,9 +5861,7 @@ static int tg_unthrottle_up(struct task_group *tg, void *data)
 		/* Add cfs_rq with already running entity in the list */
 		if (cfs_rq->nr_running >= 1)
 			list_add_leaf_cfs_rq(cfs_rq);
-#ifdef CONFIG_GROUP_IDENTITY
-		id_make_up_nr_running(tg, rq, cfs_rq->nr_tasks);
-#endif
+		id_update_make_up(tg, rq, cfs_rq->nr_tasks);
 	}
 
 	return 0;
@@ -5874,9 +5876,7 @@ static int tg_throttle_down(struct task_group *tg, void *data)
 	if (!cfs_rq->throttle_count) {
 		cfs_rq->throttled_clock_task = rq_clock_task(rq);
 		list_del_leaf_cfs_rq(cfs_rq);
-#ifdef CONFIG_GROUP_IDENTITY
-		id_make_up_nr_running(tg, rq, -(cfs_rq->nr_tasks));
-#endif
+		id_update_make_up(tg, rq, -(cfs_rq->nr_tasks));
 	}
 	cfs_rq->throttle_count++;
 
@@ -5920,6 +5920,8 @@ static void throttle_cfs_rq(struct cfs_rq *cfs_rq)
 
 	if (!se)
 		sub_nr_running(rq, task_delta);
+
+	id_commit_make_up(rq, !se);
 
 	cfs_rq->throttled = 1;
 	cfs_rq->throttled_clock = rq_clock(rq);
@@ -5997,6 +5999,8 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 
 	if (!se)
 		add_nr_running(rq, task_delta);
+
+	id_commit_make_up(rq, !se);
 
 	/* Determine whether we need to wake up potentially idle CPU: */
 	if (rq->curr == rq->idle && rq->cfs.nr_running)
@@ -6250,15 +6254,8 @@ static void check_enqueue_throttle(struct cfs_rq *cfs_rq)
 
 	/* update runtime allocation */
 	account_cfs_rq_runtime(cfs_rq, 0);
-	if (cfs_rq->runtime_remaining <= 0) {
-#ifdef CONFIG_GROUP_IDENTITY
-		rq_of(cfs_rq)->skip_make_up = true;
-#endif
+	if (cfs_rq->runtime_remaining <= 0)
 		throttle_cfs_rq(cfs_rq);
-#ifdef CONFIG_GROUP_IDENTITY
-		rq_of(cfs_rq)->skip_make_up = false;
-#endif
-	}
 }
 
 static void sync_throttle(struct task_group *tg, int cpu)

kernel/sched/sched.h
@@ -863,7 +863,8 @@ struct rq {
 	unsigned int		nr_high_running;
 	unsigned int		nr_under_running;
 	unsigned int		nr_expel_immune;
-	bool			skip_make_up;
+	long			nr_high_make_up;
+	long			nr_under_make_up;
 	bool			smt_expeller;
 	bool			smt_expellee;
 	bool			on_expel;

commit ca2c5c8844a41fba6a1482d7f687720f900f03fb

alinux: sched: fix the bug that compile faid without CONFIG_GROUP_IDENTITY
fix #34030593

In commit 8711d908e693, cfs_rq->nr_tasks
is called without #ifdef CONFIG_GROUP_IDENTITY, which will result in
compile failure if CONFIG_GROUP_IDENTITY is off. To fix this problem,
we changed the parameters of id_update_make_up().

Fixes: 8711d908e693 ("alinux: sched: make up nr_high/under_running for cfs bandwidth")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Acked-by: Michael Wang <yun.wang@linux.alibaba.com>

kernel/sched/fair.c
@@ -960,15 +960,16 @@ id_idle_cpu(struct task_struct *p, int cpu, bool expellee, bool *idle)
 }
 
 static noinline void
-id_update_make_up(struct task_group *tg, struct rq *rq, long delta)
+id_update_make_up(struct task_group *tg, struct rq *rq, struct cfs_rq *cfs_rq,
+		  int coefficient)
 {
 	struct sched_entity *se = tg->se[cpu_of(rq)];
 
 	if (is_highclass(se))
-		rq->nr_high_make_up += delta;
+		rq->nr_high_make_up += coefficient * cfs_rq->nr_tasks;
 
 	if (is_underclass(se))
-		rq->nr_under_make_up += delta;
+		rq->nr_under_make_up += coefficient * cfs_rq->nr_tasks;
 }
 
 static noinline void
@@ -1763,7 +1764,8 @@ id_preempt_all(struct sched_entity *curr, struct sched_entity *se)
 }
 
 static inline void
-id_update_make_up(struct task_group *tg, struct rq *rq, long delta)
+id_update_make_up(struct task_group *tg, struct rq *rq, struct cfs_rq *cfs_rq,
+		  int coefficient)
 {
 }
 
@@ -5861,7 +5863,7 @@ static int tg_unthrottle_up(struct task_group *tg, void *data)
 		/* Add cfs_rq with already running entity in the list */
 		if (cfs_rq->nr_running >= 1)
 			list_add_leaf_cfs_rq(cfs_rq);
-		id_update_make_up(tg, rq, cfs_rq->nr_tasks);
+		id_update_make_up(tg, rq, cfs_rq, 1);
 	}
 
 	return 0;
@@ -5876,7 +5878,7 @@ static int tg_throttle_down(struct task_group *tg, void *data)
 	if (!cfs_rq->throttle_count) {
 		cfs_rq->throttled_clock_task = rq_clock_task(rq);
 		list_del_leaf_cfs_rq(cfs_rq);
-		id_update_make_up(tg, rq, -(cfs_rq->nr_tasks));
+		id_update_make_up(tg, rq, cfs_rq, -1);
 	}
 	cfs_rq->throttle_count++;
 