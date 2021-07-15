cae7faffed46fe3135fce1b2eae2ecf4396ae524
Meng Shen <shenmeng@linux.alibaba.com>
2021-07-15 20:24:33 +0800

    ck: UKFEF: report bad mm_struct state
    to #34868789
    
    Add bad mm_struct state into unified kernel fault event framework.
    
    Signed-off-by: Wetp Zhang <wetp.zy@linux.alibaba.com>
    Signed-off-by: Meng Shen <shenmeng@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,6 +96,7 @@
 #include <linux/kasan.h>
 #include <linux/scs.h>
 #include <linux/io_uring.h>
+#include <linux/fault_event.h>
 
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -652,14 +653,20 @@ static void check_mm(struct mm_struct *mm)
 	for (i = 0; i < NR_MM_COUNTERS; i++) {
 		long x = atomic_long_read(&mm->rss_stat.count[i]);
 
-		if (unlikely(x))
+		if (unlikely(x)) {
+			report_fault_event(-1, NULL, FATAL_FAULT,
+				FE_MM_STATE, "Bad rss-counter");
 			pr_alert("BUG: Bad rss-counter state mm:%p type:%s val:%ld\n",
 				 mm, resident_page_types[i], x);
+		}
 	}
 
-	if (mm_pgtables_bytes(mm))
+	if (mm_pgtables_bytes(mm)) {
+		report_fault_event(-1, NULL, FATAL_FAULT,
+			FE_MM_STATE, "non-zero pgtables_bytes");
 		pr_alert("BUG: non-zero pgtables_bytes on freeing mm: %ld\n",
 				mm_pgtables_bytes(mm));
+	}
 
 #if defined(CONFIG_TRANSPARENT_HUGEPAGE) && !USE_SPLIT_PMD_PTLOCKS
 	VM_BUG_ON_MM(mm->pmd_huge_pte, mm);
