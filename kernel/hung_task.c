7e1850268686afe84e63714c271db4ef5ca2f721
Meng shen <shenmeng@linux.alibaba.com>
2021-07-21 18:28:28 +0800

    anolis: UKFEF: report hung-task event
    OpenAnolis Bug Tracker: 0000563
    
    Add hung-task event into unified kernel fault event framework.
    
    Signed-off-by: Wetp Zhang <wetp.zy@linux.alibaba.com>
    Signed-off-by: Meng shen <shenmeng@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- kernel/hung_task.c
+++ kernel/hung_task.c
@@ -19,6 +19,7 @@
 #include <linux/utsname.h>
 #include <linux/sched/signal.h>
 #include <linux/sched/debug.h>
+#include <linux/fault_event.h>
 
 #include <trace/events/sched.h>
 
@@ -117,6 +118,8 @@ static void check_hung_task(struct task_struct *t, unsigned long timeout)
 		hung_task_show_lock = true;
 		hung_task_call_panic = true;
 	}
+
+	report_fault_event(-1, t, NORMAL_FAULT, FE_HUNGTASK, NULL);
 
 	/*
 	 * Ok, the task did not get scheduled for more than 2 minutes,
