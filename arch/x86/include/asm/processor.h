8de236716c29e92edfb7f5ed5b34dc085e50a7e8
Zhu Yanhai <gaoyang.zyh@taobao.com>
2021-08-10 19:21:36 +0800

    x86/CPU: Adapt assembly for PIE support
    to #33971655
    
    cherry-picked from:
    https://patchwork.kernel.org/project/kernel-hardening/patch/20200228000105.165012-7-thgarnie@chromium.org/
    
    Change the assembly code to use only relative references of symbols for the
    kernel to be PIE compatible.
    
    Signed-off-by: Thomas Garnier <thgarnie@chromium.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Zhu Yanhai <gaoyang.zyh@taobao.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -731,11 +731,13 @@ static inline void sync_core(void)
 		"pushfq\n\t"
 		"mov %%cs, %0\n\t"
 		"pushq %q0\n\t"
-		"pushq $1f\n\t"
+		"leaq 1f(%%rip), %q0\n\t"
+		"pushq %q0\n\t"
 		"iretq\n\t"
 		UNWIND_HINT_RESTORE
 		"1:"
-		: "=&r" (tmp), ASM_CALL_CONSTRAINT : : "cc", "memory");
+		: "=&r" (tmp), ASM_CALL_CONSTRAINT
+		: : "cc", "memory");
 #endif
 }
 
