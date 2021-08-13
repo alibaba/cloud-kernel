6fd4d68732714741db6a60f19efb7b79b83905f9
Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
2021-08-13 15:55:07 +0800

    crypto: testmgr - Add GCM/CCM mode test of SM4 algorithm
    to #36467568
    
    commit 68039d605f7bb34ea6dbd4e099bf98599d52b0ac upstream
    
    The GCM/CCM mode of the SM4 algorithm is defined in the rfc 8998
    specification, and the test case data also comes from rfc 8998.
    
    Signed-off-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: YiLin.Li <YiLin.Li@linux.alibaba.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -4466,6 +4466,12 @@ static const struct alg_test_desc alg_test_descs[] = {
 			.hash = __VECS(aes_cbcmac_tv_template)
 		}
 	}, {
+		.alg = "cbcmac(sm4)",
+		.test = alg_test_hash,
+		.suite = {
+			.hash = __VECS(sm4_cbcmac_tv_template)
+		}
+	}, {
 		.alg = "ccm(aes)",
 		.generic_driver = "ccm_base(ctr(aes-generic),cbcmac(aes-generic))",
 		.test = alg_test_aead,
@@ -4473,6 +4479,16 @@ static const struct alg_test_desc alg_test_descs[] = {
 		.suite = {
 			.aead = {
 				____VECS(aes_ccm_tv_template),
+				.einval_allowed = 1,
+			}
+		}
+	}, {
+		.alg = "ccm(sm4)",
+		.generic_driver = "ccm_base(ctr(sm4-generic),cbcmac(sm4-generic))",
+		.test = alg_test_aead,
+		.suite = {
+			.aead = {
+				____VECS(sm4_ccm_tv_template),
 				.einval_allowed = 1,
 			}
 		}
@@ -4508,6 +4524,12 @@ static const struct alg_test_desc alg_test_descs[] = {
 		.test = alg_test_hash,
 		.suite = {
 			.hash = __VECS(des3_ede_cmac64_tv_template)
+		}
+	}, {
+		.alg = "cmac(sm4)",
+		.test = alg_test_hash,
+		.suite = {
+			.hash = __VECS(sm4_cmac128_tv_template)
 		}
 	}, {
 		.alg = "compress_null",
@@ -4953,6 +4975,13 @@ static const struct alg_test_desc alg_test_descs[] = {
 		.fips_allowed = 1,
 		.suite = {
 			.aead = __VECS(aes_gcm_tv_template)
+		}
+	}, {
+		.alg = "gcm(sm4)",
+		.generic_driver = "gcm_base(ctr(sm4-generic),ghash-generic)",
+		.test = alg_test_aead,
+		.suite = {
+			.aead = __VECS(sm4_gcm_tv_template)
 		}
 	}, {
 		.alg = "ghash",
