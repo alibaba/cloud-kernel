e4807089905eeb2a3ee92f8065894e44e9705ec7
Rongwei Wang <rongwei.wang@linux.alibaba.com>
2021-08-19 13:58:44 +0800

    anolis: mm, thp: hugetext: make PIC binary mapping address THP align
    OpenAnolis Bug Tracker: 0000366
    
    The patch mainly to make mmap address of PIC binary is aligned
    with HPAGE_PMD_SIZE. If not so, the ELF binary that is generated
    with -fPIC compile option can not use hugepages, because of
    the mapping address is randomly selected by kernel.
    
    Signed-off-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Gang Deng <gavin.dg@linux.alibaba.com>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -1006,6 +1006,11 @@ static int load_elf_binary(struct linux_binprm *bprm)
 				retval = -EINVAL;
 				goto out_free_dentry;
 			}
+#ifdef CONFIG_HUGETEXT
+			if (hugetext_enabled() && elf_interpreter &&
+					total_size >= HPAGE_PMD_SIZE)
+				load_bias &= HPAGE_PMD_MASK;
+#endif
 		}
 
 		error = elf_map(bprm->file, load_bias + vaddr, elf_ppnt,
