fdd898e1b592b0315e806d0f8410efbcd719c2b5
Tianchen Ding <dtcccc@linux.alibaba.com>
2021-09-23 10:30:15 +0800

    anolis: sched: set WRITE_ONCE on calculating load_sum when updating cfs rq load
    OpenAnolis Bug Tracker: 0000292
    
    The upstream commit 20285dc2711c96a7fc1a72707823c580702edde6 changed
    caculation method of load_sum, util_sum and runnable_sum, but forgot to
    keep WRITE_ONCE, causing a regression of about 20% in
    hackbench/datasize_4096_throughput.
    
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Acked-by: Michael Wang <yun.wang@linux.alibaba.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5013,15 +5013,15 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq)
 
 		r = removed_load;
 		sub_positive(&sa->load_avg, r);
-		sa->load_sum = sa->load_avg * divider;
+		WRITE_ONCE(sa->load_sum, sa->load_avg * divider);
 
 		r = removed_util;
 		sub_positive(&sa->util_avg, r);
-		sa->util_sum = sa->util_avg * divider;
+		WRITE_ONCE(sa->util_sum, sa->util_avg * divider);
 
 		r = removed_runnable;
 		sub_positive(&sa->runnable_avg, r);
-		sa->runnable_sum = sa->runnable_avg * divider;
+		WRITE_ONCE(sa->runnable_sum, sa->runnable_avg * divider);
 
 		/*
 		 * removed_runnable is the unweighted version of removed_load so we
