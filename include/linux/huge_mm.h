6a05301e59608e3eed7e5f6636df23ffd66b151b
Rongwei Wang <rongwei.wang@linux.alibaba.com>
2021-09-30 11:01:57 +0800

    anolis: mm, thp: hugetext: redesign fine-grained hugetext_enabled
    OpenAnolis Bug Tracker: 0000366
    
    The original 'hugetext_enabled' would make text use huge pages. The
    support text includes the code segment of binary file and anonymous vma
    which has execuable attribute. But some situations only want to enable
    hugetext for the binary file's the code segment.
    
    This patch mainly ready to separate 'hugetext_enabled'. And now, the
    'hugetext_enabled' could been written into '0, 1 2, 3' to enable or
    disable the various hugetext. The usage after fine-grained redesigning
    as following:
    
    1. enable hugetext for the code segment:
    	$ echo 1 > /sys/kernel/mm/transparent_hugepage/hugetext_enabled
    2. enable hugetext for 'rwxp' vma:
    	$ echo 2 > /sys/kernel/mm/transparent_hugepage/hugetext_enabled
    3. enable hugetext for all:
    	$ echo 3 > /sys/kernel/mm/transparent_hugepage/hugetext_enabled
    4. disable hugetext for all:
    	$ echo 0 > /sys/kernel/mm/transparent_hugepage/hugetext_enabled
    
    Signed-off-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Gang Deng <gavin.dg@linux.alibaba.com>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -63,7 +63,8 @@ enum transparent_hugepage_flag {
 #endif
 	TRANSPARENT_HUGEPAGE_FAST_COW_FLAG,
 #ifdef CONFIG_HUGETEXT
-	TRANSPARENT_HUGEPAGE_HUGETEXT_ENABLED_FLAG,
+	TRANSPARENT_HUGEPAGE_FILE_TEXT_ENABLED_FLAG,
+	TRANSPARENT_HUGEPAGE_ANON_TEXT_ENABLED_FLAG,
 #endif
 };
 
@@ -123,7 +124,8 @@ static inline bool transhuge_vma_enabled(struct vm_area_struct *vma,
 #ifdef CONFIG_HUGETEXT
 #define hugetext_enabled()			\
 	(transparent_hugepage_flags &		\
-	 (1<<TRANSPARENT_HUGEPAGE_HUGETEXT_ENABLED_FLAG))
+	 ((1<<TRANSPARENT_HUGEPAGE_FILE_TEXT_ENABLED_FLAG) |	\
+	 (1<<TRANSPARENT_HUGEPAGE_ANON_TEXT_ENABLED_FLAG)))
 
 extern unsigned long hugetext_get_unmapped_area(struct file *filp,
 		unsigned long addr, unsigned long len, unsigned long pgoff,
