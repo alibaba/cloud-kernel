9b8f50e7fb4d96b2c61dd4aa4fe79d6106222983
Thomas Gleixner <tglx@linutronix.de>
2021-10-13 16:55:33 +0200

    x86/KVM: Convert to fpstate
    ANBZ: #173
    
    commit 1c57572d754fc54e0b8ac0df5350969ce6292d12 upstream.
    
    Convert KVM code to the new register storage mechanism in preparation for
    dynamically sized buffers.
    
    No functional change.
    
    Intel-SIG: commit 1c57572d754f x86/KVM: Convert to fpstate.
    Move register state into a container struct which carries information about
    the fpstate buffer, this is the second part of a 4 part submission which
    work towards AMX and dynamic feature enabling.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Cc: kvm@vger.kernel.org
    Link: https://lkml.kernel.org/r/20211013145322.451439983@linutronix.de
    [ Lin Wang: amend commit log ]
    Signed-off-by: Lin Wang <lin.x.wang@intel.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -9744,7 +9744,7 @@ int kvm_arch_vcpu_ioctl_get_fpu(struct kvm_vcpu *vcpu, struct kvm_fpu *fpu)
 
 	vcpu_load(vcpu);
 
-	fxsave = &vcpu->arch.guest_fpu->state.fxsave;
+	fxsave = &vcpu->arch.guest_fpu->fpstate->regs.fxsave;
 	memcpy(fpu->fpr, fxsave->st_space, 128);
 	fpu->fcw = fxsave->cwd;
 	fpu->fsw = fxsave->swd;
@@ -9764,7 +9764,7 @@ int kvm_arch_vcpu_ioctl_set_fpu(struct kvm_vcpu *vcpu, struct kvm_fpu *fpu)
 
 	vcpu_load(vcpu);
 
-	fxsave = &vcpu->arch.guest_fpu->state.fxsave;
+	fxsave = &vcpu->arch.guest_fpu->fpstate->regs.fxsave;
 
 	memcpy(fxsave->st_space, fpu->fpr, 128);
 	fxsave->cwd = fpu->fcw;
