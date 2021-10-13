b9e8699f3ce99137aa984eebb68e3eff0779cef8
Thomas Gleixner <tglx@linutronix.de>
2021-10-13 16:55:45 +0200

    x86/process: Move arch_thread_struct_whitelist() out of line
    ANBZ: #173
    
    commit 2dd8eedc80b184bb16aad697ae60367c5bf07299 upstream.
    
    In preparation for dynamically enabled FPU features move the function
    out of line as the goal is to expose less and not more information.
    
    Intel-SIG: commit 2dd8eedc80b1 x86/process: Move
    arch_thread_struct_whitelist() out of line.
    Move register state into a container struct which carries information about
    the fpstate buffer, this is the second part of a 4 part submission which
    work towards AMX and dynamic feature enabling.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20211013145322.869001791@linutronix.de
    [ Lin Wang: amend commit log ]
    Signed-off-by: Lin Wang <lin.x.wang@intel.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -477,9 +477,6 @@ DECLARE_PER_CPU_ALIGNED(struct stack_canary, stack_canary);
 DECLARE_PER_CPU(struct irq_stack *, softirq_stack_ptr);
 #endif	/* X86_64 */
 
-extern unsigned int fpu_kernel_xstate_size;
-extern unsigned int fpu_user_xstate_size;
-
 struct perf_event;
 
 struct thread_struct {
@@ -554,12 +551,12 @@ struct thread_struct {
 	 */
 };
 
-/* Whitelist the FPU register state from the task_struct for hardened usercopy. */
+extern void fpu_thread_struct_whitelist(unsigned long *offset, unsigned long *size);
+
 static inline void arch_thread_struct_whitelist(unsigned long *offset,
 						unsigned long *size)
 {
-	*offset = offsetof(struct thread_struct, fpu.__fpstate.regs);
-	*size = fpu_kernel_xstate_size;
+	fpu_thread_struct_whitelist(offset, size);
 }
 
 static inline void
