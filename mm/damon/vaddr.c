aeaa2a145ef35ef9186517193b2f0276fbb58ffb
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2021-11-05 13:47:07 -0700

    mm/damon/vaddr: constify static mm_walk_ops
    ANBZ: #268
    
    commit 199b50f4c9485c46c2403d8b3e0eca90ec401ed6 upstream
    
    The only usage of these structs is to pass their addresses to
    walk_page_range(), which takes a pointer to const mm_walk_ops as
    argument.  Make them const to allow the compiler to put them in
    read-only memory.
    
    Link: https://lkml.kernel.org/r/20211014075042.17174-2-rikard.falkeborn@gmail.com
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Reviewed-by: SeongJae Park <sj@kernel.org>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Reviewed-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- mm/damon/vaddr.c
+++ mm/damon/vaddr.c
@@ -395,7 +395,7 @@ out:
 	return 0;
 }
 
-static struct mm_walk_ops damon_mkold_ops = {
+static const struct mm_walk_ops damon_mkold_ops = {
 	.pmd_entry = damon_mkold_pmd_entry,
 };
 
@@ -491,7 +491,7 @@ out:
 	return 0;
 }
 
-static struct mm_walk_ops damon_young_ops = {
+static const struct mm_walk_ops damon_young_ops = {
 	.pmd_entry = damon_young_pmd_entry,
 };
 
