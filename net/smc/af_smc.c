4aa585240451e91161a59e602918e4abd5e59734
Tony Lu <tonylu@linux.alibaba.com>
2021-12-08 20:33:42 +0800

    anolis: net/smc: Introduce SMC-R-related proc files
    ANBZ: #264
    
    This patch introduces SMC-R proc files to report statistics
    information of SMC-R connections.
    
    Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Acked-by: Dust Li <dust.li@linux.alibaba.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -52,6 +52,7 @@
 #include "smc_close.h"
 #include "smc_stats.h"
 #include "smc_tracepoint.h"
+#include "smc_proc.h"
 
 static DEFINE_MUTEX(smc_server_lgr_pending);	/* serialize link group
 						 * creation on server
@@ -86,11 +87,13 @@ int smc_hash_sk(struct sock *sk)
 	struct smc_hashinfo *h = sk->sk_prot->h.smc_hash;
 	struct hlist_head *head;
 
-	head = &h->ht;
-
 	write_lock_bh(&h->lock);
+
+	head = &h->ht[h->bkt_idx++ & (SMC_HTABLE_SIZE - 1)];
+
 	sk_add_node(sk, head);
 	sock_prot_inuse_add(sock_net(sk), sk->sk_prot, 1);
+
 	write_unlock_bh(&h->lock);
 
 	return 0;
@@ -2861,7 +2864,7 @@ static struct pernet_operations smc_net_stat_ops = {
 
 static int __init smc_init(void)
 {
-	int rc;
+	int rc, i;
 	int max_rshare, max_wshare;
 	unsigned long limit;
 
@@ -2928,19 +2931,28 @@ static int __init smc_init(void)
 		pr_err("%s: sock_register fails with %d\n", __func__, rc);
 		goto out_proto6;
 	}
-	INIT_HLIST_HEAD(&smc_v4_hashinfo.ht);
-	INIT_HLIST_HEAD(&smc_v6_hashinfo.ht);
+
+	for (i = 0; i < SMC_HTABLE_SIZE; i++) {
+		INIT_HLIST_HEAD(&smc_v4_hashinfo.ht[i]);
+		INIT_HLIST_HEAD(&smc_v6_hashinfo.ht[i]);
+	}
+
+	rc = smc_proc_init();
+	if (rc) {
+		pr_err("%s: smc_proc_init fails with %d\n", __func__, rc);
+		goto out_sock;
+	}
 
 	rc = smc_ib_register_client();
 	if (rc) {
 		pr_err("%s: ib_register fails with %d\n", __func__, rc);
-		goto out_sock;
+		goto out_proc;
 	}
 
 	rc = tcp_register_ulp(&smc_ulp_ops);
 	if (rc) {
 		pr_err("%s: tcp_ulp_register fails with %d\n", __func__, rc);
-		goto out_sock;
+		goto out_proc;
 	}
 
 	limit = nr_free_buffer_pages() << (PAGE_SHIFT - 7);
@@ -2958,6 +2970,8 @@ static int __init smc_init(void)
 	static_branch_enable(&tcp_have_smc);
 	return 0;
 
+out_proc:
+	smc_proc_exit();
 out_sock:
 	sock_unregister(PF_SMC);
 out_proto6:
@@ -2984,6 +2998,7 @@ static void __exit smc_exit(void)
 {
 	static_branch_disable(&tcp_have_smc);
 	tcp_unregister_ulp(&smc_ulp_ops);
+	smc_proc_exit();
 	sock_unregister(PF_SMC);
 	smc_core_exit();
 	smc_ib_unregister_client();
